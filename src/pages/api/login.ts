84b902e84070ea8264730cc900822179c8cf0c30
Kyle Davis <kyledavisdev@gmail.com>
2024-04-24 23:45:15 -0500

    Attempting to connect to DB

--- src/pages/api/login.ts
+++ src/pages/api/login.ts
@@ -1,16 +1,23 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
+import { db } from "./utils/db";
+import Permissions from "./models/Permissions";
 
 type Data = {
   name: string;
 };
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>,
 ) {
   if (req.method === "POST") {
     console.log(req.body);
+
+    const results = await db.from("permissions").select();
+    const data = results.data as Permissions[];
+
+    console.log(data);
+
     res.status(200).json({ name: "John Doe" });
   } else {
     // Handle any other HTTP method
