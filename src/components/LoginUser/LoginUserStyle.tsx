e853d58b08eabc1444883888f300b04112845b3f
Kyle Davis <kyledavisdev@gmail.com>
2024-04-24 23:45:47 -0500

    formatter

--- src/components/LoginUser/LoginUserStyle.tsx
+++ src/components/LoginUser/LoginUserStyle.tsx
@@ -1,30 +1,17 @@
 import styled from "../../theme/styled-components";
-import { Button } from "../../components/Button/Button";
 
 export const StyledContainer = styled.div`
-display: flex;
-flex-direction: column;
-justify-content: center;
-height: 100vh;`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  height: 100vh;
+`;
 
 export const StyledFormContainer = styled.div`
   padding: 1.5rem;
   display: flex;
   flex-direction: row;
   flex-wrap: wrap;
-    
-`;
-
-export const StyledButton = styled(Button)`
-  text-align: center;
-`;
-StyledButton.displayName = "Button";
-
-export const StyledText = styled.p`
-  text-align: center;
-  width: 100%;
-  max-width: 80%;
-  margin: 0.5em auto;
 `;
 
 export const StyledFooterDivs = styled.div`
@@ -32,10 +19,10 @@ export const StyledFooterDivs = styled.div`
   flex-direction: row;
   justify-content: space-around;
   align-items: center;
+  margin-top: 15px;
 
   > div > label {
     font-size: 0.85rem;
     text-transform: inherit;
   }
 `;
-
