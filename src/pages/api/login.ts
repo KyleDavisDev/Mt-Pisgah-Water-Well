6139e8acf7f9d5f2b303f1cb73bb888de121b491
Kyle Davis <kyledavisdev@gmail.com>
2024-04-25 21:53:13 -0500

    Validate user password, create jwt, return jwt

--- src/pages/api/login.ts
+++ src/pages/api/login.ts
@@ -1,9 +1,32 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import { cookies } from "next/headers";
 import { db } from "./utils/db";
-import Permissions from "./models/Permissions";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import Users from "./models/Users";
+
+const pwConcat = process.env.PASSWORD_CONCAT;
+const saltRounds = process.env.SALT_ROUNDS;
+const jwtPrivateKey = process.env.JWT_PRIVATE_KEY;
 
 type Data = {
-  name: string;
+  message?: string;
+  error?: string;
+};
+
+const getUser = async (username: string): Promise<Users> => {
+  const results = await db
+    .from("users")
+    .select()
+    .eq("username", username)
+    .limit(1);
+  const data = results.data as Users[];
+
+  if (data.length !== 1) {
+    throw new Error("Invalid username or password.");
+  }
+
+  return data[0];
 };
 
 export default async function handler(
@@ -11,16 +34,41 @@ export default async function handler(
   res: NextApiResponse<Data>,
 ) {
   if (req.method === "POST") {
-    console.log(req.body);
+    const { username, password } = JSON.parse(req.body);
 
-    const results = await db.from("permissions").select();
-    const data = results.data as Permissions[];
+    if (!username || !password) {
+      return res
+        .status(400)
+        .json({ error: "Must provide username and password." });
+    }
 
-    console.log(data);
+    if (!saltRounds || !pwConcat) {
+      return res.status(500).json({ error: "We messed up." });
+    }
 
-    res.status(200).json({ name: "John Doe" });
+    try {
+      const user = await getUser(username);
+
+      const match = await bcrypt.compare(password + pwConcat, user.password);
+
+      if (match) {
+        const token = jwt.sign(
+          { username: user.username },
+          jwtPrivateKey || "",
+          { algorithm: "HS256" },
+        );
+
+        res.setHeader("set-cookie", `jwt=${token} HttpOnly SameSite=Strict`);
+        return res.status(200).json({ message: "Success!" });
+      }
+    } catch (error) {
+      console.log(error);
+      return res.status(403).json({ error: "Invalid username or password." });
+    }
+
+    return res.status(500).json({ error: "Something went wrong." });
   } else {
     // Handle any other HTTP method
   }
-  res.status(200).json({ name: "John Doe" });
+  return res.status(400).json({ error: "Unsupported request type." });
 }
