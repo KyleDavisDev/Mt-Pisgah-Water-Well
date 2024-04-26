18313570b41b5925a3930cce090788e97d5ace98
Kyle Davis <kyledavisdev@gmail.com>
2024-04-25 22:02:38 -0500

    init commit

---
+++ src/components/pages/AdminDashboard/AdminDashboard.tsx
@@ -0,0 +1,13 @@
+import { Article } from "../../Article/Article";
+
+const AdminDashboard = () => {
+  return (
+    <div>
+      <Article>
+        <p>Hello world!</p>
+      </Article>
+    </div>
+  );
+};
+
+export default AdminDashboard;
