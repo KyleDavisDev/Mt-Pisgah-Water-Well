9209f9e432a4713e3075a142d4750e45fb79f318
Kyle Davis <kyledavisdev@gmail.com>
2024-04-25 20:17:33 -0500

    Update title

--- src/components/Meta/Meta.tsx
+++ src/components/Meta/Meta.tsx
@@ -1,15 +1,16 @@
 import Head from "next/head";
+
 interface IMetaProps {}
 
 const Meta = () => {
-    return (
-        <Head>
-            <meta name="viewport" content="width=device-width, initial-scale=1" />
-            <meta charSet="utf-8" />
-            <link rel="shortcut icon" href="/static/favicon.png" />
-            <title>Sauce For Your Thoughts!</title>
-        </Head>
-    );
+  return (
+    <Head>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta charSet="utf-8" />
+      <link rel="shortcut icon" href="/static/favicon.png" />
+      <title>Mt Pisgah Well</title>
+    </Head>
+  );
 };
 
 export default Meta;
