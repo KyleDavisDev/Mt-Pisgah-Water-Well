6b8be13300ac4e322bcd4084014ffdffcf83f988
Kyle Davis <kyledavisdev@gmail.com>
2024-04-27 14:08:17 -0500

    Add menu item with expandable submenu

--- src/components/pages/AdminDashboard/AdminDashboard.tsx
+++ src/components/pages/AdminDashboard/AdminDashboard.tsx
@@ -1,9 +1,10 @@
 import { Article } from "../../Article/Article";
 import styled from "styled-components";
 import { Link } from "../../Link/Link";
+import { useState } from "react";
 
 const StyledDiv = styled.div`
-  background-color: ${(props) => props.theme.siteBackgroundColorDark};
+  background-color: ${(props) => props.theme.siteBackgroundColor};
   display: flex;
   flex-direction: row;
   width: 100%;
@@ -13,8 +14,12 @@ const StyledDiv = styled.div`
 
 const StyledSideBarContainer = styled.div`
   display: flex;
+  flex-direction: column;
+
   width: 100%;
   max-width: 300px;
+  background-color: ${(props) => props.theme.white};
+  box-shadow: rgba(44, 51, 73, 0.1) 0px 0.5rem 1rem 0px;
 `;
 
 const StyledIconContainer = styled.div`
@@ -25,7 +30,29 @@ const StyledIconContainer = styled.div`
   width: 100%;
 `;
 
+const StyledMenuItemContainer = styled.div`
+  padding-left: 0;
+`;
+
+const StyledMenuItem = styled.div`
+  padding: 15px;
+  border-bottom: 1px solid rgb(237, 241, 247);
+  user-select: none;
+
+  &:hover {
+    cursor: pointer;
+    color: ${(props) => props.theme.primaryThemeColor};
+  }
+`;
+
+const StyledSubMenuItemContainer = styled.div`
+  list-style-type: none;
+  padding-left: 15px;
+`;
+
 const AdminDashboard = () => {
+  const [showHomeowners, setShowHomeowners] = useState<boolean>(false);
+
   return (
     <StyledDiv>
       <StyledSideBarContainer>
@@ -33,13 +60,30 @@ const AdminDashboard = () => {
           <Link href={"/admin/dashboard"}>
             <img
               src={"https://mtpisgahwell.com/well_icon.png"}
-              style={{ height: "75px" }}
+              style={{ height: "75px", backgroundColor: "black" }}
+              alt={"Well Icon"}
             />
           </Link>
         </StyledIconContainer>
-        <ul>
-          <li>test</li>
-        </ul>
+        <StyledMenuItemContainer>
+          <StyledMenuItem onClick={() => setShowHomeowners(!showHomeowners)}>
+            Homeowners
+          </StyledMenuItem>
+          {showHomeowners && (
+            <>
+              <StyledSubMenuItemContainer>
+                <Link href={"/homeowners/add"} inverseColors={true}>
+                  <StyledMenuItem>Add Homeowner</StyledMenuItem>
+                </Link>
+              </StyledSubMenuItemContainer>
+              <StyledSubMenuItemContainer>
+                <Link href={"/homeowners/remove"} inverseColors={true}>
+                  <StyledMenuItem>Remove Homeowner</StyledMenuItem>
+                </Link>
+              </StyledSubMenuItemContainer>
+            </>
+          )}
+        </StyledMenuItemContainer>
       </StyledSideBarContainer>
 
       <Article>
