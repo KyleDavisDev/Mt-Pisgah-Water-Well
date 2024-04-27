b1feef311351ef55e83015660c78c1011eb689bc
Kyle Davis <kyledavisdev@gmail.com>
2024-04-27 14:07:54 -0500

    Format, allow inversecolors for more usability

--- src/components/Link/Link.tsx
+++ src/components/Link/Link.tsx
@@ -3,38 +3,44 @@ import Link from "next/link";
 import styled from "../../theme/styled-components";
 
 export interface LinkProps {
-    children: string | React.JSX.Element | (string | React.JSX.Element)[];
-    href: string;
-    className?: string;
-    target?: "_blank" | "_self";
+  children: string | React.JSX.Element | (string | React.JSX.Element)[];
+  href: string;
+  className?: string;
+  target?: "_blank" | "_self";
+  inverseColors?: boolean;
 }
 
-const LinkComponent: React.FunctionComponent<LinkProps> = props => {
-    return (
-        <>
-            {props.target === "_blank" ? (
-                <a href={props.href} target="_blank" className={props.className}>
-                    {props.children}
-                </a>
-            ) : (
-                <Link href={props.href} legacyBehavior>
-                    <a className={props.className} target="_self">
-                        {props.children}
-                    </a>
-                </Link>
-            )}
-        </>
-    );
+const LinkComponent: React.FunctionComponent<LinkProps> = (
+  props: LinkProps,
+) => {
+  return (
+    <>
+      {props.target === "_blank" ? (
+        <a href={props.href} target="_blank" className={props.className}>
+          {props.children}
+        </a>
+      ) : (
+        <Link href={props.href} legacyBehavior>
+          <a className={props.className} target="_self">
+            {props.children}
+          </a>
+        </Link>
+      )}
+    </>
+  );
 };
 
 const StyledLink = styled(LinkComponent)`
   text-decoration: none;
   font-family: FuturaMedium;
-  color: ${props => props.theme.primaryThemeColor};
+  color: ${(props) =>
+    props.inverseColors
+      ? props.theme.siteFontColor
+      : props.theme.primaryThemeColor};
 
   &:hover,
   &:focus {
-    color: ${props => props.theme.secondaryThemeColor};
+    color: ${(props) => props.theme.secondaryThemeColor};
   }
 `;
 StyledLink.displayName = "Link";
