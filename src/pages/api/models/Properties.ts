d43f025e8f97d3e44381b4ba8bc86f79780537c9
Kyle Davis <kyledavisdev@gmail.com>
2024-04-28 11:40:12 -0500

    Correct model

--- src/pages/api/models/Properties.ts
+++ src/pages/api/models/Properties.ts
@@ -1,8 +1,8 @@
 export default interface Properties {
-    id: string
-    address: string
-    description: string
-    homeownerId?: number
-    createdAt: string
-    isActive?: boolean
+  id: string;
+  address: string;
+  description?: string;
+  homeownerId: number;
+  createdAt: string;
+  is_active: boolean;
 }
