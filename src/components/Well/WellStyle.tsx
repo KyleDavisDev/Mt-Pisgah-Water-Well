8745d73249d674e7b65fcdddbb6be5b1fea99e3b
Kyle Davis <kyledavisdev@gmail.com>
2024-04-27 20:38:05 -0500

    Cleanup, utilize theme colors

--- src/components/Well/WellStyle.tsx
+++ src/components/Well/WellStyle.tsx
@@ -1,18 +1,20 @@
 import styled from "../../theme/styled-components";
 
 export const StyledDiv = styled.div`
-    max-width: 2000px;
-    width: 100%;
-    margin: 0 0;
-    border: ${props => props.theme.border};
-    display: flex;
-    flex-direction: column;
-    transition: all 0.2 ease;
-    padding: 16px;
-    justify-content: flex-start;
+  max-width: 2000px;
+  width: 100%;
+  margin: 0 0;
+  border: ${(props) => props.theme.border};
+  background-color: ${(props) => props.theme.white};
+  border-radius: ${(props) => props.theme.borderRadiusSize};
+  display: flex;
+  flex-direction: column;
+  transition: all 0.2 ease;
+  padding: 16px;
+  justify-content: flex-start;
 
-    a:last-child {
-        margin: auto auto 0;
-    }
+  a:last-child {
+    margin: auto auto 0;
+  }
 `;
 StyledDiv.displayName = "div";
