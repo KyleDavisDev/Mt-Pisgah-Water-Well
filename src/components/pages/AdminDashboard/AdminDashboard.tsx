669ff81ddc83303a7b2570098f86d19363bfee42
Kyle Davis <kyledavisdev@gmail.com>
2024-05-01 18:04:34 -0500

    Add more sections

--- src/components/pages/AdminDashboard/AdminDashboard.tsx
+++ src/components/pages/AdminDashboard/AdminDashboard.tsx
@@ -5,6 +5,8 @@ import { useState } from "react";
 import HomeownersAdd from "./components/HomeownersAdd/HomeownersAdd";
 import PropertyAdd from "./components/PropertyAdd/PropertyAdd";
 import PropertyView from "./components/PropertyView/PropertyView";
+import HomeownersView from "./components/HomeownersView/HomeownersView";
+import UsageAdd from "./components/UsageAdd/UsageAdd";
 
 const StyledDiv = styled.div`
   background-color: ${(props) => props.theme.siteBackgroundColor};
@@ -17,8 +19,11 @@ const StyledDiv = styled.div`
 
 const AdminDashboard = () => {
   const [showAddHomeowner, setShowAddHomeowner] = useState(false);
+  const [showViewHomeowner, setShowViewHomeowner] = useState(false);
   const [showAddProperty, setShowAddProperty] = useState(false);
   const [showViewProperty, setShowViewProperty] = useState(false);
+  const [showAddUsage, setShowAddUsage] = useState(false);
+  const [showViewUsage, setShowViewUsage] = useState(false);
 
   const onMenuItemClick = (item: string): void => {
     if (item === "add_homeowner") {
@@ -26,6 +31,20 @@ const AdminDashboard = () => {
 
       setShowAddProperty(false);
       setShowViewProperty(false);
+      setShowViewHomeowner(false);
+      setShowAddUsage(false);
+      setShowViewUsage(false);
+      return;
+    }
+
+    if (item === "view_homeowner") {
+      setShowViewHomeowner(true);
+
+      setShowAddHomeowner(false);
+      setShowAddProperty(false);
+      setShowViewProperty(false);
+      setShowAddUsage(false);
+      setShowViewUsage(false);
       return;
     }
 
@@ -34,6 +53,9 @@ const AdminDashboard = () => {
 
       setShowAddHomeowner(false);
       setShowViewProperty(false);
+      setShowViewHomeowner(false);
+      setShowAddUsage(false);
+      setShowViewUsage(false);
       return;
     }
 
@@ -42,6 +64,31 @@ const AdminDashboard = () => {
 
       setShowAddProperty(false);
       setShowAddHomeowner(false);
+      setShowViewHomeowner(false);
+      setShowAddUsage(false);
+      setShowViewUsage(false);
+      return;
+    }
+
+    if (item === "add_usage") {
+      setShowAddUsage(true);
+
+      setShowAddProperty(false);
+      setShowAddHomeowner(false);
+      setShowViewProperty(false);
+      setShowViewHomeowner(false);
+      setShowViewUsage(false);
+      return;
+    }
+
+    if (item === "view_usage") {
+      setShowViewUsage(true);
+
+      setShowViewProperty(false);
+      setShowAddProperty(false);
+      setShowAddHomeowner(false);
+      setShowViewHomeowner(false);
+      setShowAddUsage(false);
       return;
     }
   };
@@ -52,8 +99,11 @@ const AdminDashboard = () => {
 
       <Article size="md">
         {showAddHomeowner && <HomeownersAdd />}
+        {showViewHomeowner && <HomeownersView />}
         {showAddProperty && <PropertyAdd />}
         {showViewProperty && <PropertyView />}
+        {showAddUsage && <UsageAdd />}
+        {showViewUsage && <PropertyView />}
       </Article>
     </StyledDiv>
   );
