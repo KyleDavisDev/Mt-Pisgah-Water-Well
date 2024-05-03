cc2573bbcc78814c059d99a9ac6e74bd3ff142c2
Kyle Davis <kyledavisdev@gmail.com>
2024-05-02 20:11:06 -0500

    Import child component

--- src/components/pages/AdminDashboard/components/HomeownersView/HomeownersView.tsx
+++ src/components/pages/AdminDashboard/components/HomeownersView/HomeownersView.tsx
@@ -9,15 +9,28 @@ import {
   FlashMessage,
   FlashMessageProps,
 } from "../../../../FlashMessage/FlashMessage";
+import { Button } from "../../../../Button/Button";
+
+import HomeownerEditModal from "./components/HomeownerEditModal/HomeownerEditModal";
+
+export interface homeownerVM {
+  name: string;
+  email: string;
+  phone: string;
+  mailingAddress: string;
+  isActive: boolean;
+  id: number;
+}
 
 const HomeownersView = () => {
   const _defaultErrorMessage =
     "There was a problem saving the property. Please refresh your page and try again!";
 
   // assign state
-  const [homeowners, setHomeowners] = React.useState<
-    { name: string; mailingAddress: string; isActive: boolean }[]
-  >([]);
+  const [homeowner, setHomeowner] = React.useState<homeownerVM[]>([]);
+  const [activeHomeowner, setActiveHomeowner] = React.useState<homeownerVM>();
+  const [showModal, setShowModal] = React.useState(false);
+  const [loading, setLoading] = React.useState(false);
   const [flashMessage, setFlashMessage] = React.useState<FlashMessageProps>({
     isVisible: false,
     text: "",
@@ -25,25 +38,32 @@ const HomeownersView = () => {
   });
 
   React.useEffect(() => {
-    // Fetch data from the API using a GET request
-    fetch("/api/homeowners/get", { method: "GET" })
-      .then((response) => {
-        // Check if the response is successful
-        if (!response.ok) {
-          throw new Error("Failed to fetch data");
-        }
-        // Parse the JSON response
-        return response.json();
-      })
-      .then((data) => {
-        // Update state with the fetched data
-        setHomeowners(data.homeowners);
-      })
-      .catch((error) => {
-        // Handle fetch errors
-        console.error("Error fetching data:", error);
-      });
-  }, []);
+    if (!loading && homeowner.length === 0) {
+      setLoading(true);
+      // Fetch data from the API using a GET request
+      fetch(`/api/homeowners/get?id=${1}`, { method: "GET" })
+        .then((response) => {
+          console.log("hi");
+          // Check if the response is successful
+          if (!response.ok) {
+            throw new Error("Failed to fetch data");
+          }
+          // Parse the JSON response
+          return response.json();
+        })
+        .then((data) => {
+          // Update state with the fetched data
+          setHomeowner(data.homeowners);
+        })
+        .catch((error) => {
+          // Handle fetch errors
+          console.error("Error fetching data:", error);
+        })
+        .finally(() => {
+          setLoading(false);
+        });
+    }
+  }, [loading]);
 
   const onFlashClose = () => {
     // clear flash message if was shown
@@ -54,51 +74,9 @@ const HomeownersView = () => {
     });
   };
 
-  // const onSubmit = async (event: React.FormEvent): Promise<any> => {
-  //   event.preventDefault();
-  //
-  //   if (!address) {
-  //     setFlashMessage({
-  //       isVisible: true,
-  //       text: "Missing address",
-  //       type: "alert",
-  //     });
-  //     return;
-  //   }
-  //
-  //   try {
-  //     const response = await fetch("/api/properties/add", {
-  //       method: "POST",
-  //       body: JSON.stringify({
-  //         address,
-  //         description,
-  //         homeowner,
-  //       }),
-  //     });
-  //
-  //     if (response.ok) {
-  //       const data = await response.json();
-  //
-  //       setFlashMessage({
-  //         isVisible: true,
-  //         text: data.message,
-  //         type: "success",
-  //       });
-  //
-  //       setAddress("");
-  //       setDescription("");
-  //       setHomeowner(properties[0].id);
-  //     }
-  //   } catch (err: any) {
-  //     console.log(err);
-  //     // Create warning flash
-  //     setFlashMessage({
-  //       isVisible: true,
-  //       text: err.response?.data?.msg || _defaultErrorMessage,
-  //       type: "warning",
-  //     });
-  //   }
-  // };
+  const onModalClose = () => {
+    setShowModal(false);
+  };
 
   return (
     <StyledContainer>
@@ -120,21 +98,41 @@ const HomeownersView = () => {
                 <th>Address</th>
                 <th>Description</th>
                 <th>Active</th>
+                <th></th>
               </tr>
             </thead>
             <tbody>
-              {homeowners &&
-                homeowners.map((homeowner) => {
+              {homeowner &&
+                homeowner.map((homeowner, ind) => {
+                  if (ind > 0) return null;
+
                   return (
-                    <tr>
+                    <tr key={`tr__${ind}__${homeowner.isActive}`}>
                       <td>{homeowner.name}</td>
                       <td>{homeowner.mailingAddress}</td>
                       <td>{homeowner.isActive}</td>
+                      <td style={{ textAlign: "center" }}>
+                        <Button
+                          onClick={() => {
+                            setShowModal(!showModal);
+                            setActiveHomeowner(homeowner);
+                          }}
+                        >
+                          Edit
+                        </Button>
+                      </td>
                     </tr>
                   );
                 })}
             </tbody>
           </StyledTable>
+          {activeHomeowner && (
+            <HomeownerEditModal
+              showModal={showModal}
+              homeowner={{ ...activeHomeowner }}
+              onModalClose={onModalClose}
+            />
+          )}
         </StyledFormContainer>
       </Well>
     </StyledContainer>
