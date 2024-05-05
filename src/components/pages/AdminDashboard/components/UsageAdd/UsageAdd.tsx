d45ba5a5515325112092bad23276d895f1e491a5
Kyle Davis <kyledavisdev@gmail.com>
2024-05-05 14:21:55 -0500

    WIP render usages

--- src/components/pages/AdminDashboard/components/UsageAdd/UsageAdd.tsx
+++ src/components/pages/AdminDashboard/components/UsageAdd/UsageAdd.tsx
@@ -1,19 +1,68 @@
-import { StyledWellContainer, StyledFooterDivs, StyledFormContainer, StyledContainer } from "./UsageAddStyle";
+import {
+  StyledWellContainer,
+  StyledFooterDivs,
+  StyledFormContainer,
+  StyledContainer,
+  StyledTable
+} from "./UsageAddStyle";
 import Well from "../../../../Well/Well";
 import { FlashMessage, FlashMessageProps } from "../../../../FlashMessage/FlashMessage";
-import { TextInput } from "../../../../TextInput/TextInput";
 import { Button } from "../../../../Button/Button";
 import React from "react";
 import { Article } from "../../../../Article/Article";
+import styled from "../../../../../theme/styled-components";
+import { TextInput } from "../../../../TextInput/TextInput";
+
+const StyledTd = styled.td`
+  padding: 0 !important;
+  border-bottom: 0 !important;
+
+  div input {
+    width: 50%;
+    margin-bottom: 0;
+    margin-top: 0;
+  }
+
+  > div {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 55px;
+    padding: 8px;
+    border-bottom: 1px solid #dddddd;
+  }
+
+  > div:last-child {
+    padding: 8px;
+    border-bottom: 0;
+  }
+`;
+
+const StyledFakeTd = styled.div``;
+const StyledLastFakeTd = styled.td`
+  padding: 0 !important;
+  border-bottom: 0 !important;
+`;
+
+interface UsageVM {
+  id: string;
+  name: string;
+  properties: {
+    id: string;
+    address: string;
+    description: string;
+    usages: {
+      id: string;
+      gallons: string;
+    }[];
+  }[];
+}
 
 const UsageAdd = () => {
   const _defaultErrorMessage = "There was a problem saving the usage. Please refresh your page and try again!";
 
   // assign state
-  const [gallons, setGallons] = React.useState("");
-  const [address, setAddress] = React.useState("");
-  const [properties, setProperties] = React.useState<{ address: string; description: string; isActive: boolean }[]>([]);
-  const [homeowner, setHomeowner] = React.useState("");
+  const [homeowners, setHomeowners] = React.useState<UsageVM[]>([]);
   const [flashMessage, setFlashMessage] = React.useState<FlashMessageProps>({
     isVisible: false,
     text: "",
@@ -22,7 +71,7 @@ const UsageAdd = () => {
 
   React.useEffect(() => {
     // Fetch data from the API using a GET request
-    fetch("/api/homeowners/get", { method: "GET" })
+    fetch("/api/usages/getForAdding", { method: "GET" })
       .then(response => {
         // Check if the response is successful
         if (!response.ok) {
@@ -33,7 +82,7 @@ const UsageAdd = () => {
       })
       .then(data => {
         // Update state with the fetched data
-        setProperties(data.properties);
+        setHomeowners(data.homeowners);
       })
       .catch(error => {
         // Handle fetch errors
@@ -52,54 +101,55 @@ const UsageAdd = () => {
 
   const onSubmit = async (event: React.FormEvent): Promise<any> => {
     event.preventDefault();
-
-    if (!address) {
-      setFlashMessage({
-        isVisible: true,
-        text: "Missing address",
-        type: "alert"
-      });
-      return;
-    }
-
-    try {
-      const response = await fetch("/api/usages/add", {
-        method: "POST",
-        body: JSON.stringify({
-          address,
-          description: gallons,
-          homeowner
-        })
-      });
-
-      if (response.ok) {
-        const data = await response.json();
-
-        setFlashMessage({
-          isVisible: true,
-          text: data.message,
-          type: "success"
-        });
-
-        setAddress("");
-        setGallons("");
-      }
-    } catch (err: any) {
-      console.log(err);
-      // Create warning flash
-      setFlashMessage({
-        isVisible: true,
-        text: err.response?.data?.msg || _defaultErrorMessage,
-        type: "warning"
-      });
-    }
+    return;
+    //   if (!address) {
+    //     setFlashMessage({
+    //       isVisible: true,
+    //       text: "Missing address",
+    //       type: "alert"
+    //     });
+    //     return;
+    //   }
+    //
+    //   try {
+    //     const response = await fetch("/api/usages/add", {
+    //       method: "POST",
+    //       body: JSON.stringify({
+    //         address,
+    //         description: gallons,
+    //         homeowner
+    //       })
+    //     });
+    //
+    //     if (response.ok) {
+    //       const data = await response.json();
+    //
+    //       setFlashMessage({
+    //         isVisible: true,
+    //         text: data.message,
+    //         type: "success"
+    //       });
+    //
+    //       setAddress("");
+    //       setGallons("");
+    //     }
+    //   } catch (err: any) {
+    //     console.log(err);
+    //     // Create warning flash
+    //     setFlashMessage({
+    //       isVisible: true,
+    //       text: err.response?.data?.msg || _defaultErrorMessage,
+    //       type: "warning"
+    //     });
+    //   }
   };
 
   return (
     <StyledContainer>
-      <Article size="md">
+      <Article size="lg">
         <StyledWellContainer>
           <Well>
+            <h3>Add Usage</h3>
             <StyledFormContainer>
               {flashMessage.isVisible && (
                 <FlashMessage type={flashMessage.type} isVisible onClose={onFlashClose}>
@@ -108,29 +158,61 @@ const UsageAdd = () => {
               )}
 
               <form onSubmit={e => onSubmit(e)} style={{ width: "100%" }}>
-                <TextInput
-                  onChange={e => setAddress(e.target.value)}
-                  value={address}
-                  type={"text"}
-                  id={"address"}
-                  showLabel={true}
-                  label={"Address"}
-                  name={"address"}
-                  required={true}
-                />
-                <TextInput
-                  onChange={e => setGallons(e.target.value)}
-                  value={gallons}
-                  type={"text"}
-                  id={"description"}
-                  showLabel={true}
-                  label={"Description"}
-                  name={"description"}
-                />
+                <StyledTable>
+                  <thead>
+                    <tr>
+                      <th>Homeowner</th>
+                      <th>Property</th>
+                      <th>Previous Usage</th>
+                      <th>New Usage</th>
+                      <th>Delta</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {homeowners &&
+                      homeowners.map(homeowner => {
+                        return (
+                          <tr key={`homeowner_${homeowner.id}`}>
+                            <td>
+                              <h4>{homeowner.name}</h4>
+                            </td>
+                            <StyledTd>
+                              {homeowner.properties.map(property => {
+                                return <div key={`property_${property.id}`}>{property.address}</div>;
+                              })}
+                            </StyledTd>
+                            <StyledTd>
+                              {homeowner.properties.map(property => {
+                                if (!property.usages) return <div>0</div>;
+                                if (!property.usages[0]) return <div>0</div>;
 
+                                return (
+                                  <div key={`previous_usage_${property.usages[0].id}`}>
+                                    {property.usages[0].gallons}
+                                  </div>
+                                );
+                              })}
+                            </StyledTd>
+                            <StyledTd>
+                              {homeowner.properties.map(property => {
+                                return (
+                                  <TextInput key={`new_usage_${property.id}`} id={property.id} onChange={() => {}} />
+                                );
+                              })}
+                            </StyledTd>
+                            <StyledTd>
+                              {homeowner.properties.map(p => {
+                                return <div>123</div>;
+                              })}
+                            </StyledTd>
+                          </tr>
+                        );
+                      })}
+                  </tbody>
+                </StyledTable>
                 <StyledFooterDivs>
                   <Button type="submit" fullWidth>
-                    Add Usages
+                    Save Usages
                   </Button>
                 </StyledFooterDivs>
               </form>
