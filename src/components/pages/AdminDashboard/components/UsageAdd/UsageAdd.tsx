b1633ab7e0d79058aa6b171f382f4166513f0548
Kyle Davis <kyledavisdev@gmail.com>
2024-05-05 17:35:53 -0500

    Update styling, update delta column, update logic on submit

--- src/components/pages/AdminDashboard/components/UsageAdd/UsageAdd.tsx
+++ src/components/pages/AdminDashboard/components/UsageAdd/UsageAdd.tsx
@@ -1,10 +1,10 @@
 import {
-  StyledWellContainer,
+  StyledContainer,
   StyledFooterDivs,
   StyledFormContainer,
-  StyledContainer,
   StyledTable,
-  StyledTd
+  StyledTd,
+  StyledWellContainer
 } from "./UsageAddStyle";
 import Well from "../../../../Well/Well";
 import { FlashMessage, FlashMessageProps } from "../../../../FlashMessage/FlashMessage";
@@ -32,6 +32,9 @@ const UsageAdd = () => {
 
   // assign state
   const [homeowners, setHomeowners] = React.useState<UsageVM[]>([]);
+  const [usages, setUsages] = React.useState<{ [key: string]: { previous: string; new: string } }>({});
+  const [deltas, setDeltas] = React.useState<{ [key: string]: number }>({});
+  const [dateCollected, setDateCollected] = React.useState("2024-05-22");
   const [flashMessage, setFlashMessage] = React.useState<FlashMessageProps>({
     isVisible: false,
     text: "",
@@ -51,7 +54,27 @@ const UsageAdd = () => {
       })
       .then(data => {
         // Update state with the fetched data
+
+        const usagesCollector: { [key: string]: { previous: string; new: string } } = {};
+        const deltasCollector: { [key: string]: number } = {};
+        for (let i = 0; i < data.homeowners.length; i++) {
+          const homeowner = data.homeowners[i];
+          for (let j = 0; j < homeowner.properties.length; j++) {
+            const property = homeowner.properties[j];
+            if (usagesCollector[property.id.toString()] === undefined) {
+              usagesCollector[property.id.toString()] = {
+                previous: property.usages[0] ? property.usages[0].gallons : 0,
+                new: ""
+              };
+              deltasCollector[property.id.toString()] = 0;
+            }
+          }
+        }
+
         setHomeowners(data.homeowners);
+        setUsages(usagesCollector);
+        setDeltas(deltasCollector);
+        setDateCollected(getFormattedFirstDayOfMonth());
       })
       .catch(error => {
         // Handle fetch errors
@@ -68,49 +91,92 @@ const UsageAdd = () => {
     });
   };
 
+  const safeParseStr = (str: string) => {
+    try {
+      const num = parseInt(str, 10); // Parse the string to an integer with base 10
+      if (isNaN(num)) {
+        return 0;
+      }
+      return num;
+    } catch (error) {
+      return 0;
+    }
+  };
+
+  let getFormattedFirstDayOfMonth = () => {
+    const now = new Date();
+    const year = now.getFullYear();
+    const month = (now.getMonth() + 1).toString().padStart(2, "0");
+    const firstDayOfMonth = "01";
+
+    // Format the first day of the month as 'yyyy-mm-dd'
+    return `${year}-${month}-${firstDayOfMonth}`;
+  };
+
+  const updateDeltas = (id: string) => {
+    const newDeltas = { ...deltas };
+    const newUsage = safeParseStr(usages[id].new);
+    const previousUsage = safeParseStr(usages[id].previous);
+    newDeltas[id] = newUsage - previousUsage;
+    setDeltas(newDeltas);
+    setFlashMessage({
+      isVisible: false,
+      text: "",
+      type: undefined
+    });
+  };
+
   const onSubmit = async (event: React.FormEvent): Promise<any> => {
     event.preventDefault();
-    return;
-    //   if (!address) {
-    //     setFlashMessage({
-    //       isVisible: true,
-    //       text: "Missing address",
-    //       type: "alert"
-    //     });
-    //     return;
-    //   }
-    //
-    //   try {
-    //     const response = await fetch("/api/usages/add", {
-    //       method: "POST",
-    //       body: JSON.stringify({
-    //         address,
-    //         description: gallons,
-    //         homeowner
-    //       })
-    //     });
-    //
-    //     if (response.ok) {
-    //       const data = await response.json();
-    //
-    //       setFlashMessage({
-    //         isVisible: true,
-    //         text: data.message,
-    //         type: "success"
-    //       });
-    //
-    //       setAddress("");
-    //       setGallons("");
-    //     }
-    //   } catch (err: any) {
-    //     console.log(err);
-    //     // Create warning flash
-    //     setFlashMessage({
-    //       isVisible: true,
-    //       text: err.response?.data?.msg || _defaultErrorMessage,
-    //       type: "warning"
-    //     });
-    //   }
+
+    for (const key in deltas) {
+      const delta = deltas[key];
+
+      if (delta < 0) {
+        setFlashMessage({
+          isVisible: true,
+          text: "Cannot have a negative usage.",
+          type: "alert"
+        });
+        return;
+      }
+    }
+
+    try {
+      const response = await fetch("/api/usages/add", {
+        method: "POST",
+        body: JSON.stringify({
+          usages: Object.keys(usages)
+            .filter(key => {
+              return safeParseStr(usages[key].new) > 0;
+            })
+            .map(key => {
+              return {
+                id: key,
+                usage: usages[key].new
+              };
+            })
+        })
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+
+        setFlashMessage({
+          isVisible: true,
+          text: data.message,
+          type: "success"
+        });
+      }
+    } catch (err: any) {
+      console.log(err);
+      // Create warning flash
+      setFlashMessage({
+        isVisible: true,
+        text: err.response?.data?.msg || _defaultErrorMessage,
+        type: "warning"
+      });
+    }
   };
 
   return (
@@ -127,14 +193,28 @@ const UsageAdd = () => {
               )}
 
               <form onSubmit={e => onSubmit(e)} style={{ width: "100%" }}>
+                <div style={{ maxWidth: "380px", width: "100%" }}>
+                  <TextInput
+                    onChange={e => {
+                      console.log(e.currentTarget.value);
+                      setDateCollected(e.currentTarget.value);
+                    }}
+                    id={"dateCollected"}
+                    type={"date"}
+                    label={"Date Collected"}
+                    required={true}
+                    showLabel={true}
+                    value={dateCollected}
+                  />
+                </div>
                 <StyledTable>
                   <thead>
                     <tr>
                       <th>Homeowner</th>
                       <th>Property</th>
-                      <th>Previous Usage</th>
-                      <th>New Usage</th>
-                      <th>Delta</th>
+                      <th>Previous Reading</th>
+                      <th>New Reading</th>
+                      <th>Usage</th>
                     </tr>
                   </thead>
                   <tbody>
@@ -151,27 +231,46 @@ const UsageAdd = () => {
                               })}
                             </StyledTd>
                             <StyledTd>
-                              {homeowner.properties.map(property => {
-                                if (!property.usages) return <div>0</div>;
-                                if (!property.usages[0]) return <div>0</div>;
+                              {homeowner.properties.map((property, index) => {
+                                let value = "0";
 
-                                return (
-                                  <div key={`previous_usage_${property.usages[0].id}`}>
-                                    {property.usages[0].gallons}
-                                  </div>
-                                );
+                                if (!property.usages) value = "0";
+                                else if (!property.usages[0]) value = "0";
+                                else value = property.usages[0].gallons;
+
+                                return <div key={`previous_usage_${index}`}>{value}</div>;
                               })}
                             </StyledTd>
                             <StyledTd>
                               {homeowner.properties.map(property => {
                                 return (
-                                  <TextInput key={`new_usage_${property.id}`} id={property.id} onChange={() => {}} />
+                                  <TextInput
+                                    key={`new_usage_${property.id}`}
+                                    id={property.id}
+                                    value={usages[property.id].new}
+                                    onChange={e => {
+                                      const inputValue = e.currentTarget.value;
+
+                                      const newUsages = { ...usages };
+                                      newUsages[property.id] = {
+                                        ...newUsages[property.id],
+                                        new: inputValue.replace(/\D+/, "")
+                                      };
+                                      setUsages(newUsages);
+                                    }}
+                                    onBlur={() => updateDeltas(property.id)}
+                                  />
                                 );
                               })}
                             </StyledTd>
                             <StyledTd>
                               {homeowner.properties.map(p => {
-                                return <div>123</div>;
+                                const color = deltas[p.id] > 0 ? "green" : "red";
+                                return (
+                                  <div key={`difference_${p.id}`} style={{ color }}>
+                                    {deltas[p.id].toLocaleString()}
+                                  </div>
+                                );
                               })}
                             </StyledTd>
                           </tr>
