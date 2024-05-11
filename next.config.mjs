ffb11ac2cf4fdcaede20a4e98bd6ff283b77a526
Kyle Davis <kyledavisdev@gmail.com>
2024-05-11 16:48:14 -0500

    add redirect

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,18 @@
-/** @type {import('next').NextConfig} */
+/** @type {import("next").NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  async redirects() {
+    return [
+      // Basic redirect
+      {
+        source: "/admin",
+        destination: "/admin/dashboard",
+        permanent: true
+      },
+    ];
+  }
+
 };
 
 export default nextConfig;
