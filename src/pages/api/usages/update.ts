4b8805dadaab7b77b29a08fc90db06c92205cb10
Kyle Davis <kyledavisdev@gmail.com>
2024-06-04 22:36:20 -0500

    init

---
+++ src/pages/api/usages/update.ts
@@ -0,0 +1,40 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { db } from "../utils/db";
+import { validateCookie, validatePermission } from "../utils/utils";
+
+type Data = {
+  message?: string;
+  error?: string;
+};
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+  if (req.method === "POST") {
+    try {
+      const jwtCookie = req.cookies["jwt"];
+      const username = await validateCookie(jwtCookie);
+      await validatePermission(username, "UPDATE_USAGE");
+
+      const { id, gallons, isActive } = JSON.parse(req.body);
+
+      // TODO: data validation
+
+      await db
+        .from("usages")
+        .update({
+          gallons,
+          is_active: isActive
+        })
+        .eq("id", id);
+
+      return res.status(200).json({ message: "Success!" });
+    } catch (error) {
+      console.log(error);
+      return res.status(403).json({ error: "Unable to update record." });
+    }
+
+    return res.status(500).json({ error: "Something went wrong." });
+  } else {
+    // Handle any other HTTP method
+  }
+  return res.status(400).json({ error: "Unsupported request type." });
+}
