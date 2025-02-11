db37af675a351f1c23e68a289dbb5b96e39d40d4
Kyle Davis <kyledavisdev@gmail.com>
2025-02-10 21:37:56 -0600

    cleanup

--- src/pages/api/utils/utils.ts
+++ src/pages/api/utils/utils.ts
@@ -95,10 +95,11 @@ export const addAuditTableRecord = async ({
 }): Promise<AuditLog> => {
   try {
     const record = await db<AuditLog[]>`
-    INSERT into audit_log (table_name, record_id, action_type, old_data, new_data, action_by_id, action_timestamp)
-        VALUES (${tableName}, ${recordId}, ${actionType}, ${oldData ?? null}, ${newData ?? null}, (SELECT id from users where username = ${actionBy}), now())
-    returning *;
-  `;
+        INSERT into audit_log (table_name, record_id, action_type, old_data, new_data, action_by_id, action_timestamp)
+            VALUES (${tableName}, ${recordId}, ${actionType}, ${oldData ?? null}, ${newData ?? null}, (SELECT id from users where username = ${actionBy}), now())
+        
+        returning *;
+    `;
 
     if (!record) throw new Error("Audit log insertion failed: No record returned.");
 
@@ -112,19 +113,19 @@ export const addAuditTableRecord = async ({
 export const updateAuditTableRecord = async (auditLog: AuditLog): Promise<AuditLog> => {
   try {
     await db`
-    UPDATE audit_log set table_name=${auditLog.table_name},
-                         record_id=${auditLog.record_id},
-                         action_type=${auditLog.action_type},
-                         old_data=${JSON.stringify(auditLog.old_data)},
-                         new_data=${JSON.stringify(auditLog.new_data)},
-                         action_by_id=${auditLog.action_by_id},
-                         action_timestamp=${auditLog.action_timestamp}
-    where id=${auditLog.id}
-  `;
+      UPDATE audit_log set table_name=${auditLog.table_name},
+                           record_id=${auditLog.record_id},
+                           action_type=${auditLog.action_type},
+                           old_data=${JSON.stringify(auditLog.old_data)},
+                           new_data=${JSON.stringify(auditLog.new_data)},
+                           action_by_id=${auditLog.action_by_id},
+                           action_timestamp=${auditLog.action_timestamp}
+      where id=${auditLog.id}
+    `;
 
     return auditLog;
   } catch (e) {
-    console.log("Unable to insert into audit_log table");
-    throw new Error(`Could not insert audit_log record: ${e instanceof Error ? e.message : "Unknown error"}`);
+    console.log("Unable to update audit_log table");
+    throw new Error(`Could not update audit_log record: ${e instanceof Error ? e.message : "Unknown error"}`);
   }
 };
