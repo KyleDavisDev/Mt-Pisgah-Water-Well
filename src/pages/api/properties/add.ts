75cdbaf2a6176c2499d31fcefc6423581f991793
Kyle Davis <kyledavisdev@gmail.com>
2025-02-10 21:37:49 -0600

    Use updated tables, use intent-based logging

--- src/pages/api/properties/add.ts
+++ src/pages/api/properties/add.ts
@@ -1,6 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { db } from "../utils/db";
-import { getUsernameFromCookie, validatePermission } from "../utils/utils";
+import { addAuditTableRecord, getUsernameFromCookie, updateAuditTableRecord, validatePermission } from "../utils/utils";
 
 type Data = {
   message?: string;
@@ -17,12 +17,29 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
       await validatePermission(username, "ADD_HOMEOWNER");
 
       const { address, description, homeowner } = JSON.parse(req.body);
-      await db.from("properties").insert({
-        address: address,
-        description,
-        homeowner_id: homeowner,
-        is_active: true
+
+      // TODO: Data validation
+
+      const auditLog = await addAuditTableRecord({
+        tableName: "properties",
+        recordId: 0, // will update
+        newData: JSON.stringify({ address, description, homeowner, is_active: true }),
+        actionBy: username,
+        actionType: "INSERT"
       });
+
+      const record = await db`
+        INSERT into properties (address, description, homeowner_id, is_active) 
+            VALUES (${address}, ${description}, (SELECT id FROM homeowners where id=${homeowner}), true)
+        returning *;
+      `;
+
+      if (!record) {
+        return res.status(500).json({ error: "Unable to insert new properties record" });
+      }
+
+      await updateAuditTableRecord({ ...auditLog, record_id: record[0].id, is_complete: true });
+
       return res.status(200).json({ message: "Success!" });
     } catch (error) {
       console.log(error);
