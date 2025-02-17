aa3cbecc4bfda40e46ca9f72766bd3c041f7e224
Kyle Davis <kyledavisdev@gmail.com>
2025-02-16 19:17:45 -0600

    Cleanup, add anchor styleing

--- app/admin/dashboard/components/SidebarMenu/SidebarMenuStyle.tsx
+++ app/admin/dashboard/components/SidebarMenu/SidebarMenuStyle.tsx
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled from "../../../../theme/styled-components";
 
 export const StyledSideBarContainer = styled.div`
   display: flex;
@@ -6,7 +6,7 @@ export const StyledSideBarContainer = styled.div`
 
   width: 100%;
   max-width: 300px;
-  background-color: ${(props) => props.theme.white};
+  background-color: ${props => props.theme.white};
   box-shadow: rgba(44, 51, 73, 0.1) 0px 0.5rem 1rem 0px;
 `;
 
@@ -29,11 +29,26 @@ export const StyledMenuItem = styled.div`
 
   &:hover {
     cursor: pointer;
-    color: ${(props) => props.theme.primaryThemeColor};
+    color: ${props => props.theme.primaryThemeColor};
   }
 `;
 
 export const StyledSubMenuItemContainer = styled.div`
   list-style-type: none;
   padding-left: 15px;
+  border-bottom: 1px solid rgb(237, 241, 247);
+  display: flex;
+  flex-direction: row;
+
+  a {
+    text-decoration: none;
+    color: ${props => props.theme.siteFontColor};
+    width: 100%;
+    padding: 15px;
+
+    &:hover {
+      cursor: pointer;
+      color: ${props => props.theme.primaryThemeColor};
+    }
+  }
 `;
