81b81512b55ff5cfbcaa60c5f48390310862cfe3
Kyle Davis <kyledavisdev@gmail.com>
2025-02-16 19:29:03 -0600

    Init commit -- from being moved over

---
+++ app/admin/dashboard/properties/all/components/PropertyEditModal/PropertyEditModal.tsx
@@ -0,0 +1,176 @@
+"use client";
+
+import React from "react";
+import { propertyVM } from "../../page";
+import { homeownerVM } from "../../../../homeowners/all/page";
+import { StyledFooterDivs } from "../../../../homeowners/all/pageStyle";
+
+import { TextInput } from "../../../../../../components/TextInput/TextInput";
+import { Button } from "../../../../../../components/Button/Button";
+import { Modal } from "../../../../../../components/Modal/Modal";
+import { FlashMessage, FlashMessageProps } from "../../../../../../components/FlashMessage/FlashMessage";
+import { RadioButton } from "../../../../../../components/RadioButton/RadioButton";
+import Label from "../../../../../../components/Label/Label";
+import Select from "../../../../../../components/Select/Select";
+
+export interface PropertyEditModalProps {
+  showModal: boolean;
+  property: propertyVM;
+  homeowners: homeownerVM[];
+  onModalClose: () => void;
+}
+
+const PropertyEditModal = (props: PropertyEditModalProps) => {
+  const [id, setId] = React.useState(props.property.id);
+  const [description, setDescription] = React.useState(props.property.description);
+  const [isActive, setIsActive] = React.useState(props.property.isActive);
+  const [address, setAddress] = React.useState(props.property.address);
+  const homeowner = props.homeowners.find(h => h.name === props.property.homeowner);
+  const activeId = homeowner?.id ? homeowner.id.toString() : "";
+  const [activeHomeownerId, setActiveHomeownerId] = React.useState(activeId);
+
+  const [flashMessage, setFlashMessage] = React.useState<FlashMessageProps>({
+    isVisible: false,
+    text: "",
+    type: undefined
+  });
+
+  const onFlashClose = () => {
+    // clear flash message if was shown
+    setFlashMessage({
+      isVisible: false,
+      text: "",
+      type: undefined
+    });
+  };
+
+  const onSubmit = async (event?: React.FormEvent): Promise<any> => {
+    event?.preventDefault();
+
+    if (!address || !id || !activeHomeownerId) {
+      setFlashMessage({
+        isVisible: true,
+        text: "Missing address",
+        type: "alert"
+      });
+      return;
+    }
+
+    try {
+      const response = await fetch("/api/properties/update", {
+        method: "POST",
+        body: JSON.stringify({
+          description,
+          id,
+          isActive,
+          homeownerId: activeHomeownerId
+        })
+      });
+
+      if (response.ok) {
+        setFlashMessage({
+          isVisible: false,
+          text: "",
+          type: undefined
+        });
+        setDescription("");
+        setAddress("");
+
+        props.onModalClose();
+      }
+    } catch (err: any) {
+      console.log(err);
+      // Create warning flash
+      setFlashMessage({
+        isVisible: true,
+        text: err.response?.data?.msg || "Error",
+        type: "warning"
+      });
+    }
+  };
+
+  const onKeyUpEvent = (e: React.KeyboardEvent<HTMLFormElement>) => {
+    if (e.key === "Enter") {
+      onSubmit();
+    }
+  };
+
+  return (
+    <Modal isActive={props.showModal} onClose={props.onModalClose}>
+      <>
+        {flashMessage.isVisible && (
+          <FlashMessage type={flashMessage.type} isVisible onClose={onFlashClose}>
+            {flashMessage.text}
+          </FlashMessage>
+        )}
+
+        <form onKeyUp={e => onKeyUpEvent(e)} onSubmit={e => onSubmit(e)} style={{ width: "100%" }}>
+          <TextInput
+            onChange={e => setAddress(e.target.value)}
+            value={address}
+            type={"text"}
+            id={"address"}
+            showLabel={true}
+            label={"Mailing Address"}
+            name={"address"}
+            required={true}
+            disabled={true}
+          />
+
+          <TextInput
+            onChange={e => setDescription(e.target.value)}
+            value={description}
+            type={"text"}
+            id={"description"}
+            showLabel={true}
+            label={"Description"}
+            name={"description"}
+            required={false}
+          />
+
+          <Select
+            id="homeowner"
+            showLabel={true}
+            label={"Property belongs to"}
+            name={"homeowner"}
+            options={props.homeowners.map(h => ({
+              name: h.name,
+              value: h.id.toString()
+            }))}
+            onSelect={e => {
+              return setActiveHomeownerId(e.target.value);
+            }}
+            selectedValue={activeHomeownerId}
+            required={true}
+          />
+
+          <Label>Active</Label>
+          <RadioButton
+            onClick={() => setIsActive("true")}
+            name={"is_active"}
+            isChecked={isActive.toLowerCase() === "true"}
+            id={"RBisActiveYes"}
+            label={"Yes"}
+            value={"Yes"}
+          />
+          <RadioButton
+            onClick={() => setIsActive("false")}
+            name={"is_active"}
+            isChecked={isActive.toLowerCase() === "false"}
+            id={"RBisActiveNo"}
+            label={"No"}
+            value={"No"}
+          />
+
+          <StyledFooterDivs>
+            <Button type="submit" fullWidth>
+              Save
+            </Button>
+          </StyledFooterDivs>
+        </form>
+      </>
+    </Modal>
+  );
+};
+
+export default PropertyEditModal;
---
+++ app/admin/dashboard/properties/all/page.tsx
@@ -0,0 +1,221 @@
+"use client";
+
+import React from "react";
+import { StyledWellContainer, StyledFormContainer, StyledTable, StyledContainer } from "./pageStyle";
+import Well from "../../../../components/Well/Well";
+import { Button } from "../../../../components/Button/Button";
+import PropertyEditModal from "./components/PropertyEditModal/PropertyEditModal";
+import { Article } from "../../../../components/Article/Article";
+import { homeownerVM } from "../../homeowners/all/page";
+
+export interface propertyVM {
+  address: string;
+  description: string;
+  isActive: string;
+  id: string;
+  homeowner: string;
+}
+
+const page = () => {
+  // assign state
+  const [activeProperties, setActiveProperties] = React.useState<propertyVM[]>([]);
+  const [inactiveProperties, setInactiveProperties] = React.useState<propertyVM[]>([]);
+  const [homeowners, setHomeowners] = React.useState<homeownerVM[]>([]);
+
+  const [activeProperty, setActiveProperty] = React.useState<propertyVM | null>();
+  const [showModal, setShowModal] = React.useState(false);
+  const initialized = React.useRef(false);
+
+  const getProperties = () => {
+    // Fetch data from the API using a GET request
+    fetch("/api/properties", { method: "GET" })
+      .then(response => {
+        // Check if the response is successful
+        if (!response.ok) {
+          throw new Error("Failed to fetch data");
+        }
+        // Parse the JSON response
+        return response.json();
+      })
+      .then(data => {
+        // Update state with the fetched data
+
+        const active: propertyVM[] = [];
+        const inActive: propertyVM[] = [];
+
+        data.properties.forEach((p: any) => {
+          if (p.isActive === "true") {
+            active.push(p);
+          } else {
+            inActive.push(p);
+          }
+        });
+
+        setActiveProperties(active);
+        setInactiveProperties(inActive);
+      })
+      .catch(error => {
+        // Handle fetch errors
+        console.error("Error fetching data:", error);
+      })
+      .finally(() => {});
+  };
+
+  const getHomeowners = () => {
+    // Fetch data from the API using a GET request
+    fetch(`/api/homeowners/get`, { method: "GET" })
+      .then(response => {
+        // Check if the response is successful
+        if (!response.ok) {
+          throw new Error("Failed to fetch data");
+        }
+        // Parse the JSON response
+        return response.json();
+      })
+      .then(data => {
+        // Update state with the fetched data
+        setHomeowners(data.homeowners);
+      })
+      .catch(error => {
+        // Handle fetch errors
+        console.error("Error fetching data:", error);
+      })
+      .finally(() => {});
+  };
+
+  React.useEffect(() => {
+    if (!initialized.current) {
+      initialized.current = true;
+      getProperties();
+      getHomeowners();
+    }
+  }, []);
+
+  const onModalClose = () => {
+    setShowModal(false);
+    setActiveProperty(null);
+    getProperties();
+  };
+
+  return (
+    <StyledContainer>
+      <Article size="md">
+        <StyledWellContainer>
+          <Well>
+            <h3>All Properties</h3>
+            <StyledFormContainer>
+              {activeProperties.length > 0 && (
+                <>
+                  <h3>Active</h3>
+                  <StyledTable style={{ marginBottom: "25px" }}>
+                    <thead>
+                      <tr>
+                        <th>Address</th>
+                        <th>Description</th>
+                        <th>Homeowner</th>
+                        <th></th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {activeProperties.map(property => {
+                        return (
+                          <tr key={property.id}>
+                            <td>
+                              <span
+                                style={{
+                                  height: 10,
+                                  width: 10,
+                                  backgroundColor: property.isActive === "true" ? "green" : "red",
+                                  borderRadius: 50,
+                                  display: "inline-block",
+                                  marginRight: 8
+                                }}
+                              ></span>
+                              {property.address}
+                            </td>
+                            <td>{property.description}</td>
+                            <td>{property.homeowner}</td>
+                            <td style={{ textAlign: "center" }}>
+                              <Button
+                                onClick={() => {
+                                  setActiveProperty({ ...property });
+                                  setShowModal(true);
+                                }}
+                              >
+                                Edit
+                              </Button>
+                            </td>
+                          </tr>
+                        );
+                      })}
+                    </tbody>
+                  </StyledTable>
+                </>
+              )}
+
+              {inactiveProperties.length > 0 && (
+                <>
+                  <h3>Inactive</h3>
+                  <StyledTable>
+                    <thead>
+                      <tr>
+                        <th>Address</th>
+                        <th>Description</th>
+                        <th>Homeowner</th>
+                        <th></th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {inactiveProperties.map(property => {
+                        return (
+                          <tr key={property.id}>
+                            <td>
+                              <span
+                                style={{
+                                  height: 10,
+                                  width: 10,
+                                  backgroundColor: property.isActive === "true" ? "green" : "red",
+                                  borderRadius: 50,
+                                  display: "inline-block",
+                                  marginRight: 8
+                                }}
+                              ></span>
+                              {property.address}
+                            </td>
+                            <td>{property.description}</td>
+                            <td>{property.homeowner}</td>
+                            <td style={{ textAlign: "center" }}>
+                              <Button
+                                onClick={() => {
+                                  setActiveProperty({ ...property });
+                                  setShowModal(true);
+                                }}
+                              >
+                                Edit
+                              </Button>
+                            </td>
+                          </tr>
+                        );
+                      })}
+                    </tbody>
+                  </StyledTable>
+                </>
+              )}
+
+              {activeProperty && (
+                <PropertyEditModal
+                  showModal={showModal}
+                  property={{ ...activeProperty }}
+                  onModalClose={onModalClose}
+                  homeowners={homeowners}
+                />
+              )}
+            </StyledFormContainer>
+          </Well>
+        </StyledWellContainer>
+      </Article>
+    </StyledContainer>
+  );
+};
+
+export default page;
