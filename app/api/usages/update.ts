6f140f20c5b211f245306dc9c0b3f5e17b74f1c7
Kyle Davis <kyledavisdev@gmail.com>
2025-03-04 21:58:59 -0600

    Modernize

--- app/api/usages/update.ts
+++ app/api/usages/update.ts
@@ -1,40 +1,62 @@
-import type { NextApiRequest, NextApiResponse } from "next";
 import { db } from "../utils/db";
-import { getUsernameFromCookie, validatePermission } from "../utils/utils";
+import { addAuditTableRecord, getUsernameFromCookie, validatePermission } from "../utils/utils";
+import { cookies } from "next/headers";
+import { getUsageById } from "../repositories/usageRepository";
 
-type Data = {
-  message?: string;
-  error?: string;
-};
+export async function PUT(req: Request) {
+  if (req.method !== "PUT") {
+    return new Response("Method Not Allowed", { status: 405 });
+  }
+  try {
+    const cookieStore = cookies();
+    const jwtCookie = cookieStore.get("jwt");
+    const username = await getUsernameFromCookie(jwtCookie);
+    await validatePermission(username, "UPDATE_USAGE");
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
-  if (req.method === "POST") {
-    try {
-      const jwtCookie = req.cookies["jwt"];
-      const username = await getUsernameFromCookie(jwtCookie);
-      await validatePermission(username, "UPDATE_USAGE");
+    const { id, gallons, isActive } = await req.json();
 
-      const { id, gallons, isActive } = JSON.parse(req.body);
-
-      // TODO: data validation
-
-      await db
-        .from("usages")
-        .update({
-          gallons,
-          is_active: isActive
-        })
-        .eq("id", id);
-
-      return res.status(200).json({ message: "Success!" });
-    } catch (error) {
-      console.log(error);
-      return res.status(403).json({ error: "Unable to update record." });
+    if (!id || !gallons || !isActive) {
+      return new Response("Missing required fields", { status: 400 });
     }
 
-    return res.status(500).json({ error: "Something went wrong." });
-  } else {
-    // Handle any other HTTP method
+    // Find record to be edited
+    const oldUsage = await getUsageById(id);
+    if (!oldUsage) return new Response("Cannot find usage record", { status: 404 });
+
+    // Get new record data
+    const newUsage = { ...oldUsage, gallons, is_active: isActive === "true" };
+
+    // log intent
+    const auditRecord = await addAuditTableRecord({
+      oldData: JSON.stringify(oldUsage),
+      newData: JSON.stringify(newUsage),
+      recordId: oldUsage.id,
+      tableName: "usages",
+      actionType: "UPDATE",
+      actionBy: username
+    });
+
+    // Make update in a transaction
+    await db.begin(async db => {
+      await db`
+          UPDATE usages
+          SET gallons   = ${newUsage.gallons},
+              is_active = ${newUsage.is_active}
+          WHERE id = ${id};
+      `;
+
+      await db`
+          UPDATE audit_log
+          SET is_complete= true
+          WHERE id = ${auditRecord.id};
+      `;
+    });
+
+    return Response.json({ message: "Success!" });
+  } catch (error) {
+    console.log(error);
+    return new Response("Invalid username or password.", { status: 403 });
   }
-  return res.status(400).json({ error: "Unsupported request type." });
+
+  return new Response("Something went wrong.", { status: 500 });
 }
