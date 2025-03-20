1d8cb64ec267e00c9656bec2fe6aec8cb8a3cdbb
Kyle Davis <kyledavisdev@gmail.com>
2025-03-20 14:57:04 -0500

    Remove unused styles, fix padding

--- app/admin/dashboard/bills/add/pageStyle.tsx
+++ app/admin/dashboard/bills/add/pageStyle.tsx
@@ -10,7 +10,6 @@ export const StyledWellContainer = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
-  height: 100vh;
 `;
 
 export const StyledFormContainer = styled.div`
@@ -18,19 +17,18 @@ export const StyledFormContainer = styled.div`
   display: flex;
   flex-direction: row;
   flex-wrap: wrap;
+
+  > div {
+    width: 100%;
+  }
 `;
 
-export const StyledFooterDivs = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-around;
-  align-items: center;
-  margin-top: 15px;
+export const StyledTableContainer = styled.div`
+  padding: 5px 0 15px;
+`;
 
-  > div > label {
-    font-size: 0.85rem;
-    text-transform: inherit;
-  }
+export const StyledTableHeader = styled.h5`
+  padding-bottom: 5px;
 `;
 
 export const StyledTable = styled.table`
@@ -53,30 +51,5 @@ export const StyledTable = styled.table`
 
   tr {
     border-bottom: 1px solid #ddd;
-  }
-`;
-
-export const StyledTd = styled.td`
-  padding: 0 !important;
-  border-bottom: 0 !important;
-
-  div input {
-    width: 50%;
-    margin-bottom: 0;
-    margin-top: 0;
-  }
-
-  > div {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 55px;
-    padding: 8px;
-    border-bottom: 1px solid #dddddd;
-  }
-
-  > div:last-child {
-    padding: 8px;
-    border-bottom: 0;
   }
 `;
