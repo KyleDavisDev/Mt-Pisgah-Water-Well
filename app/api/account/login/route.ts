bd7a5d92b6b82b3db7c81947c4a716b860ba3d11
Kyle Davis <kyledavisdev@gmail.com>
2025-03-28 16:40:47 -0500

    Cleanup, add login records

--- app/api/account/login/route.ts
+++ app/api/account/login/route.ts
@@ -1,16 +1,41 @@
-import { db } from "../../utils/db";
 import { cookies } from "next/headers";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
-import Users from "../../models/Users";
-import { getUserByUsername } from "../../utils/utils";
+import { getClientIPFromRequest, getUserAgentFromRequest, getUserByUsername } from "../../utils/utils";
+import { db } from "../../utils/db";
 
 const pwConcat = process.env.PASSWORD_CONCAT;
 const saltRounds = process.env.SALT_ROUNDS;
 const jwtPrivateKey = process.env.JWT_PRIVATE_KEY;
 
+const genericNotFoundMessage = () => new Response("Invalid username or password.", { status: 400 });
+
+interface InsertLoginLogParams {
+  username: string;
+  ip: string;
+  userAgent?: string | null;
+  failureReason?: string | null;
+}
+
+const insertLoginLogRecord = async ({
+  username,
+  ip,
+  userAgent = null,
+  failureReason = null
+}: InsertLoginLogParams): Promise<void> => {
+  await db`
+      INSERT INTO login_log (username, ip_address, user_agent, failure_reason)
+      VALUES (${username},
+              ${ip},
+              ${userAgent},
+              ${failureReason ?? null})
+  `;
+};
+
 export async function POST(req: Request): Promise<Response> {
   try {
+    let clientIPAddress = getClientIPFromRequest(req);
+    let clientUserAgent = getUserAgentFromRequest(req);
     const { username, password } = await req.json();
 
     // Basic input validation
@@ -23,18 +48,49 @@ export async function POST(req: Request): Promise<Response> {
     }
 
     let user;
+
     try {
       user = await getUserByUsername(username);
     } catch (e) {
-      return new Response("Invalid username or password.", { status: 403 });
+      const errMsg = e instanceof Error ? e.message : String(e);
+
+      await insertLoginLogRecord({
+        username,
+        failureReason: errMsg,
+        ip: clientIPAddress,
+        userAgent: clientUserAgent
+      });
+      return genericNotFoundMessage();
+    }
+
+    // Quick sanity check
+    if (!user) {
+      await insertLoginLogRecord({
+        username,
+        failureReason: "Could not find user.",
+        ip: clientIPAddress,
+        userAgent: clientUserAgent
+      });
+      return genericNotFoundMessage();
     }
 
     // Check password
     const match = await bcrypt.compare(password + pwConcat, user.password);
-
     if (!match) {
-      return new Response("Invalid username or password.", { status: 403 });
+      await insertLoginLogRecord({
+        username,
+        failureReason: "Invalid password.",
+        ip: clientIPAddress,
+        userAgent: clientUserAgent
+      });
+      return genericNotFoundMessage();
     }
+
+    await insertLoginLogRecord({
+      username,
+      ip: clientIPAddress,
+      userAgent: clientUserAgent
+    });
 
     // Sign JWT
     const token = jwt.sign({ username: user.username }, jwtPrivateKey, {
