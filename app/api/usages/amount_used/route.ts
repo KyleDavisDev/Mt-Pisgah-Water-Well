e50baf067d1de83fadbdae8ca06a2ba502de5a6b
Kyle Davis <kyledavisdev@gmail.com>
2025-03-28 14:29:24 -0500

    WIP figuring out range

--- app/api/usages/amount_used/route.ts
+++ app/api/usages/amount_used/route.ts
@@ -1,13 +1,15 @@
 import { db } from "../../utils/db";
-import { getUsernameFromCookie, validatePermission } from "../../utils/utils";
+import { extractKeyFromRequest, getUsernameFromCookie, validatePermission } from "../../utils/utils";
 import { cookies } from "next/headers";
 import Usage from "../../models/Usages";
 import Homeowner from "../../models/Homeowners";
 import Property from "../../models/Properties";
 
-const extractKeyFromQuery = (request: Request, key: string): string[] | null => {
-  const url = new URL(request.url);
-  return url.searchParams.getAll(key);
+const formatMonth = (month: number): string => {
+  if (month < 0) return "00";
+  if (month < 9) return "0" + month.toString();
+  if (month < 13) return month.toString();
+  return "01";
 };
 
 export async function GET(req: Request) {
@@ -21,8 +23,8 @@ export async function GET(req: Request) {
     const username = await getUsernameFromCookie(jwtCookie);
     await validatePermission(username, "VIEW_USAGES");
 
-    const month = extractKeyFromQuery(req, "month");
-    const year = extractKeyFromQuery(req, "year");
+    const month = extractKeyFromRequest(req, "month");
+    const year = extractKeyFromRequest(req, "year");
 
     if (!month || !year) {
       return new Response("Missing 'month' or 'year' query parameter", { status: 400 });
@@ -34,31 +36,13 @@ export async function GET(req: Request) {
 
     const startOfMonth = `${year[0]}-${month[0]}-01`;
     const endOfMonth = `${year[0]}-${month[0]}-28`;
+    const nextMonth = parseInt(month[0], 10) + 1;
 
-    console.log(startOfMonth, endOfMonth);
+    const formattedNextMonth = formatMonth(nextMonth);
+    const startOfNextMonth = `${nextMonth < 13 ? year[0] : (parseInt(year[0], 10) + 1).toString()}-${formattedNextMonth}-01`;
+    const endOfNextMonth = startOfNextMonth.substring(0, 8) + "28";
 
-    // First usage of the month
-    const firstUsages = await db<Usage[]>`
-        SELECT DISTINCT ON (u.property_id)
-            u.property_id,
-            u.gallons
-        FROM usages u
-        WHERE u.date_collected >= ${startOfMonth}
-          AND u.date_collected < ${endOfMonth}
-          AND u.is_active = true
-        ORDER BY u.property_id, u.date_collected ASC
-    `;
-
-    // Last usage of the month
-    const lastUsages = await db<Usage[]>`
-        SELECT DISTINCT ON (u.property_id) u.property_id, u.gallons
-        FROM usages u
-        WHERE u.date_collected >= ${endOfMonth}
-          AND u.is_active = true
-        ORDER BY u.property_id, u.date_collected ASC
-    `;
-
-    console.log(firstUsages, lastUsages);
+    console.log(startOfMonth, endOfMonth, startOfNextMonth, endOfNextMonth);
 
     const homeowners = await db<Homeowner[]>`
         SELECT homeowner.id, homeowner.name, homeowner.is_active
@@ -74,38 +58,55 @@ export async function GET(req: Request) {
           AND is_active = true
     `;
 
-    const returnData = homeowners.map(homeowner => {
-      return {
-        id: homeowner.id.toString(),
-        name: homeowner.name,
-        properties: properties
-          .filter(p => p.homeowner_id === homeowner.id)
-          .map(p => {
-            const firstUsage = firstUsages.find(f => f.property_id === p.id);
-            const lastUsage = lastUsages.find(l => l.property_id === p.id);
+    // First usage of the month
+    const startingUsage = await db<Usage[]>`
+        SELECT DISTINCT ON (u.property_id) u.property_id, u.gallons
+        FROM usages u
+        WHERE u.date_collected >= ${startOfMonth}
+          AND u.date_collected < ${endOfMonth}
+          AND u.is_active = true
+          AND u.property_id IN ${db(properties.map(p => p.id))}
+        ORDER BY u.property_id, u.date_collected ASC
+    `;
 
-            if (!firstUsage || !lastUsage) {
+    // Find the first usage of the next month
+    const endingUsage = await db<Usage[]>`
+        SELECT DISTINCT ON (u.property_id) u.property_id, u.gallons
+        FROM usages u
+        WHERE u.date_collected >= ${startOfNextMonth}
+          AND u.date_collected < ${endOfNextMonth}
+          AND u.is_active = true
+          AND u.property_id IN ${db(properties.map(p => p.id))}
+        ORDER BY u.property_id, u.date_collected ASC
+    `;
+
+    console.log(startingUsage);
+
+    const returnData = homeowners
+      .filter(h => {
+        return properties.some(p => p.homeowner_id === h.id);
+      })
+      .map(homeowner => {
+        return {
+          id: homeowner.id.toString(),
+          name: homeowner.name,
+          properties: properties
+            .filter(p => p.homeowner_id === homeowner.id)
+            .map(p => {
+              const firstUsage = startingUsage.find(f => f.property_id === p.id);
+              const lastUsage = endingUsage.find(l => l.property_id === p.id);
+
               return {
                 id: p.id.toString(),
                 address: p.address,
                 description: p.description,
-                startingGallons: 0,
-                endingGallons: 0,
-                gallonsUsed: 0
+                startingGallons: firstUsage ? firstUsage.gallons : "--",
+                endingGallons: lastUsage ? lastUsage.gallons : "--",
+                gallonsUsed: lastUsage && firstUsage ? lastUsage.gallons - firstUsage.gallons : "--"
               };
-            }
-
-            return {
-              id: p.id.toString(),
-              address: p.address,
-              description: p.description,
-              startingGallons: firstUsage.gallons,
-              endingGallons: lastUsage.gallons,
-              gallonsUsed: lastUsage.gallons - firstUsage.gallons
-            };
-          })
-      };
-    });
+            })
+        };
+      });
 
     return Response.json({
       homeowners: returnData
