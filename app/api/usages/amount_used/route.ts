74eaa9e17f889ef5424d208f2f8b3661934a1440
Kyle Davis <kyledavisdev@gmail.com>
2025-04-05 15:08:20 -0500

    use repository methods, cleanup

--- app/api/usages/amount_used/route.ts
+++ app/api/usages/amount_used/route.ts
@@ -1,16 +1,13 @@
-import { db } from "../../utils/db";
-import { extractKeyFromRequest, getUsernameFromCookie, validatePermission } from "../../utils/utils";
+import {
+  extractKeyFromRequest,
+  getStartAndEndOfProvidedMonthAndNextMonth,
+  getUsernameFromCookie,
+  validatePermission
+} from "../../utils/utils";
 import { cookies } from "next/headers";
-import Usage from "../../models/Usages";
-import Homeowner from "../../models/Homeowners";
-import Property from "../../models/Properties";
-
-const formatMonth = (month: number): string => {
-  if (month < 0) return "00";
-  if (month < 9) return "0" + month.toString();
-  if (month < 13) return month.toString();
-  return "01";
-};
+import { getFirstUsageByDateCollectedRangeAndPropertyIn } from "../../repositories/usageRepository";
+import { getAllActiveProperties } from "../../repositories/propertiesRepository";
+import { getAllActiveHomeowners } from "../../repositories/homeownerRepository";
 
 export async function GET(req: Request) {
   if (req.method !== "GET") {
@@ -34,54 +31,22 @@ export async function GET(req: Request) {
       return new Response("'month' and 'year' cannot be arrays", { status: 400 });
     }
 
-    const startOfMonth = `${year[0]}-${month[0]}-01`;
-    const endOfMonth = `${year[0]}-${month[0]}-28`;
-    const nextMonth = parseInt(month[0], 10) + 1;
+    const { startOfMonth, endOfMonth, startOfNextMonth, endOfNextMonth } = getStartAndEndOfProvidedMonthAndNextMonth(
+      year[0],
+      month[0]
+    );
 
-    const formattedNextMonth = formatMonth(nextMonth);
-    const startOfNextMonth = `${nextMonth < 13 ? year[0] : (parseInt(year[0], 10) + 1).toString()}-${formattedNextMonth}-01`;
-    const endOfNextMonth = startOfNextMonth.substring(0, 8) + "28";
+    const properties = await getAllActiveProperties();
+    const propertyIds = properties.map((p: any) => p.id);
 
-    console.log(startOfMonth, endOfMonth, startOfNextMonth, endOfNextMonth);
+    const startingUsages = await getFirstUsageByDateCollectedRangeAndPropertyIn(startOfMonth, endOfMonth, propertyIds);
+    const endingUsages = await getFirstUsageByDateCollectedRangeAndPropertyIn(
+      startOfNextMonth,
+      endOfNextMonth,
+      propertyIds
+    );
 
-    const homeowners = await db<Homeowner[]>`
-        SELECT homeowner.id, homeowner.name, homeowner.is_active
-        FROM homeowners homeowner
-        WHERE homeowner.is_active = true
-        ORDER BY homeowner.id
-    `;
-
-    const properties = await db<Property[]>`
-        SELECT *
-        FROM properties
-        WHERE homeowner_id IN ${db(homeowners.map(h => h.id))}
-          AND is_active = true
-    `;
-
-    // First usage of the month
-    const startingUsage = await db<Usage[]>`
-        SELECT DISTINCT ON (u.property_id) u.property_id, u.gallons
-        FROM usages u
-        WHERE u.date_collected >= ${startOfMonth}
-          AND u.date_collected < ${endOfMonth}
-          AND u.is_active = true
-          AND u.property_id IN ${db(properties.map(p => p.id))}
-        ORDER BY u.property_id, u.date_collected ASC
-    `;
-
-    // Find the first usage of the next month
-    const endingUsage = await db<Usage[]>`
-        SELECT DISTINCT ON (u.property_id) u.property_id, u.gallons
-        FROM usages u
-        WHERE u.date_collected >= ${startOfNextMonth}
-          AND u.date_collected < ${endOfNextMonth}
-          AND u.is_active = true
-          AND u.property_id IN ${db(properties.map(p => p.id))}
-        ORDER BY u.property_id, u.date_collected ASC
-    `;
-
-    console.log(startingUsage);
-
+    const homeowners = await getAllActiveHomeowners();
     const returnData = homeowners
       .filter(h => {
         return properties.some(p => p.homeowner_id === h.id);
@@ -93,8 +58,8 @@ export async function GET(req: Request) {
           properties: properties
             .filter(p => p.homeowner_id === homeowner.id)
             .map(p => {
-              const firstUsage = startingUsage.find(f => f.property_id === p.id);
-              const lastUsage = endingUsage.find(l => l.property_id === p.id);
+              const firstUsage = startingUsages.find(f => f.property_id === p.id);
+              const lastUsage = endingUsages.find(l => l.property_id === p.id);
 
               return {
                 id: p.id.toString(),
