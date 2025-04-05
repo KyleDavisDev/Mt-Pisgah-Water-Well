1b5261d7ab71076bdaa45d68e9ac91330c611e32
Kyle Davis <kyledavisdev@gmail.com>
2025-04-05 15:08:45 -0500

    Added method

--- app/api/repositories/propertiesRepository.ts
+++ app/api/repositories/propertiesRepository.ts
@@ -14,3 +14,20 @@ export const getAllActiveProperties = async (): Promise<Property[]> => {
 
   return properties ?? [];
 };
+
+/**
+ * Retrieves all active properties from the database where the property belongs to a provided homeownerId
+ * @param homeownerIds list of homeownerIds to search for
+ *
+ * @returns Promise resolving to an array of Property records
+ */
+export const getAllActivePropertiesByHomeownerIdIn = async (homeownerIds: number[]): Promise<Property[]> => {
+  const properties = await db<Property[]>`
+      SELECT *
+      FROM properties
+      WHERE homeowner_id IN ${db(homeownerIds)}
+        AND is_active = true
+  `;
+
+  return properties ?? [];
+};
