676e8fa8621a9dfc22d26b9240275693effe1845
Kyle Davis <kyledavisdev@gmail.com>
2025-04-14 22:17:06 -0500

    Add types

--- app/admin/dashboard/bills/all/types.ts
+++ app/admin/dashboard/bills/all/types.ts
@@ -1,3 +1,52 @@
+export interface BillUsage {
+  start: number;
+  end: number;
+  usage: number;
+}
+
+export interface MonthlyUsage {
+  [key: string]: BillUsage;
+}
+
+export interface BillCharges {
+  baseCharge: number;
+  excessCharge: number;
+  lateFee: number;
+  otherCharges: number;
+  amountOutstanding: number;
+  totalAmount: number;
+}
+
+export interface WaterCompanyInfo {
+  name: string;
+  address: string;
+  city: string;
+  state: string;
+  zip: string;
+  phone: string;
+  fax: string;
+  email: string;
+}
+
+export interface HomeownerInfo {
+  name: string;
+  address: string;
+  city: string;
+  state: string;
+  zip: string;
+}
+
+export interface BillDetails {
+  id: string;
+  accountDate: string;
+  billingPeriod: string;
+  waterCompany: WaterCompanyInfo;
+  homeowner: HomeownerInfo;
+  charges: BillCharges;
+  currentUsage: BillUsage;
+  monthlyUsageHistory: MonthlyUsage;
+}
+
 export interface billVM {
   id: string;
   amountInPennies: number;
