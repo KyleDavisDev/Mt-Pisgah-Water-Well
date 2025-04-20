e933734750af404e45485754faac83733f359d80
Kyle Davis <kyledavisdev@gmail.com>
2025-04-20 16:06:45 -0500

    Add DTO types, cleanup

--- app/admin/dashboard/bills/view/[id]/types.ts
+++ app/admin/dashboard/bills/view/[id]/types.ts
@@ -5,16 +5,19 @@ export interface BillUsage {
 }
 
 export interface MonthlyUsage {
-  [key: string]: BillUsage;
+  month: number;
+  year: number;
+  gallonsUsed: string;
+  amountInPennies: number;
 }
 
 export interface BillCharges {
-  baseCharge: number;
+  baseCharge: string;
   excessCharge: number;
   lateFee: number;
   otherCharges: number;
   amountOutstanding: number;
-  totalAmount: number;
+  totalAmount: string;
 }
 
 export interface WaterCompanyInfo {
@@ -30,7 +33,10 @@ export interface WaterCompanyInfo {
 
 export interface HomeownerInfo {
   name: string;
-  address: string;
+}
+
+export interface PropertyInfo {
+  street: string;
   city: string;
   state: string;
   zip: string;
@@ -38,19 +44,41 @@ export interface HomeownerInfo {
 
 export interface BillDetails {
   id: string;
-  accountDate: string;
+  createdDate: string;
   billingPeriod: string;
   waterCompany: WaterCompanyInfo;
   homeowner: HomeownerInfo;
+  property: PropertyInfo;
   charges: BillCharges;
   currentUsage: BillUsage;
-  monthlyUsageHistory: MonthlyUsage;
+  monthlyUsageHistory: MonthlyUsage[];
 }
 
-export interface billVM {
+export interface billDTO {
   id: string;
   amountInPennies: number;
-  dateCreated: string;
+  formula: any;
   gallonsUsed: string;
-  isActive: string;
+  month: number;
+  year: number;
+  isActive: boolean;
+  createdAt: string;
+}
+
+export interface homeownerDTO {
+  name: string;
+}
+
+export interface propertyDTO {
+  street: string;
+  city: string;
+  state: string;
+  zip: string;
+}
+
+export interface historicalUsageDTO {
+  month: number;
+  year: number;
+  gallonsUsed: string;
+  amountInPennies: number;
 }
