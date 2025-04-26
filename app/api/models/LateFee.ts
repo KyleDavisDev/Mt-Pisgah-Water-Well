ba605d2ab61999262e7a886bbf8eb9fe06113e14
Kyle Davis <kyledavisdev@gmail.com>
2025-04-26 12:32:56 -0500

    Remove unused files

--- app/api/models/LateFee.ts
+++
@@ -1,7 +0,0 @@
-export interface LateFee {
-  id: number;
-  usage_bill_id: number;
-  amount_in_pennies: number;
-  reason?: string | null;
-  created_at: string; // ISO timestamp format (e.g. "2025-04-14T18:00:00.000Z")
-}
