810404796446108fc43e91a8650ade4fa97da82f
Kyle Davis <kyledavisdev@gmail.com>
2025-04-26 12:18:52 -0500

    Provide formula

--- app/api/bills/[id]/route.ts
+++ app/api/bills/[id]/route.ts
@@ -1,10 +1,14 @@
 import { cookies } from "next/headers";
 
-import { getBillById, getRecentActiveBillsByPropertyBeforeBillingMonthYear } from "../../repositories/billRepository";
+import {
+  getInvoiceById,
+  getRecentActiveInvoicesByPropertyBeforeBillingMonthYear
+} from "../../repositories/invoiceRepository";
 import { getHomeownerByPropertyId } from "../../repositories/homeownerRepository";
 import { getUsernameFromCookie, validatePermission } from "../../utils/utils";
 import { getPropertyById } from "../../repositories/propertiesRepository";
 import { getLateFeesByUsageBillId } from "../../repositories/lateFeeRepository";
+import { PRICING_FORMULAS } from "../pricingFormulas";
 
 export async function GET(req: Request, { params }: { params: { id: string } }): Promise<Response> {
   if (req.method !== "GET") {
@@ -18,7 +22,7 @@ export async function GET(req: Request, { params }: { params: { id: string } }):
     const username = await getUsernameFromCookie(jwtCookie);
     await validatePermission(username, "VIEW_BILLS");
 
-    const bill = await getBillById(params.id);
+    const bill = await getInvoiceById(params.id);
     if (!bill) {
       return Response.json({ error: "Bill not found" }, { status: 404 });
     }
@@ -27,7 +31,12 @@ export async function GET(req: Request, { params }: { params: { id: string } }):
     const [homeowner, property, historicalUsages, lateFees] = await Promise.all([
       getHomeownerByPropertyId(bill.property_id),
       getPropertyById(bill.property_id),
-      getRecentActiveBillsByPropertyBeforeBillingMonthYear(bill.property_id, 12, bill.billing_month, bill.billing_year),
+      getRecentActiveInvoicesByPropertyBeforeBillingMonthYear(
+        bill.property_id,
+        12,
+        bill.billing_month,
+        bill.billing_year
+      ),
       getLateFeesByUsageBillId(bill.id)
     ]);
 
@@ -35,11 +44,18 @@ export async function GET(req: Request, { params }: { params: { id: string } }):
       return Response.json({ error: "Homeowner and Property info not found" }, { status: 404 });
     }
 
+    const formula = PRICING_FORMULAS[bill.formula_used];
+
     return Response.json({
       bill: {
         id: bill.id,
         amountInPennies: bill.amount_in_pennies,
-        formula: {},
+        formula: {
+          description: formula.description,
+          baseFeeInPennies: formula.baseFeeInPennies,
+          baseGallons: formula.baseGallons,
+          usageRateInPennies: formula.usageRateInPennies
+        },
         gallonsUsed: bill.gallons_used,
         month: bill.billing_month,
         year: bill.billing_year,
