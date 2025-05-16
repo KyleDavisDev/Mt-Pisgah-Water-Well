140f02dce6244b56f650ac6a97ebafc54cda9ae3
Kyle Davis <kyledavisdev@gmail.com>
2025-05-15 22:39:19 -0500

    init commit

---
+++ app/api/invoices/update/route.ts
@@ -0,0 +1,42 @@
+import { cookies } from "next/headers";
+import { db } from "../../utils/db";
+import { getUsernameFromCookie, validatePermission } from "../../utils/utils";
+import { getUsageById } from "../../repositories/usageRepository";
+import { addAuditTableRecord } from "../../repositories/auditRepository";
+import { getInvoiceById, updateInvoiceAsTransactional } from "../../repositories/invoiceRepository";
+
+export async function PUT(req: Request) {
+  if (req.method !== "PUT") {
+    return new Response("Method Not Allowed", { status: 405 });
+  }
+  try {
+    const cookieStore = cookies();
+    const jwtCookie = cookieStore.get("jwt");
+    const username = await getUsernameFromCookie(jwtCookie);
+    await validatePermission(username, "UPDATE_INVOICE");
+
+    const { id, isActive } = await req.json();
+
+    if (!id || !isActive) {
+      return new Response("Missing required fields", { status: 400 });
+    }
+
+    // Find the invoice to be edited
+    const oldInvoice = await getInvoiceById(id);
+    if (!oldInvoice) return new Response("Cannot find invoice record", { status: 404 });
+
+    // Set the new record data
+    const newUsage = { ...oldInvoice, is_active: isActive === "true" };
+
+    const updatedRecord = await updateInvoiceAsTransactional(username, oldInvoice, newUsage);
+
+    if (!updatedRecord) return new Response("Failed to update invoice record", { status: 500 });
+
+    return Response.json({ message: "Success!" });
+  } catch (error) {
+    console.log(error);
+    return new Response("Invalid username or password.", { status: 403 });
+  }
+
+  return new Response("Something went wrong.", { status: 500 });
+}
