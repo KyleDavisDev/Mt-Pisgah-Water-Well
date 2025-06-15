137af0fd646a92cc0c8c894bfb82f3893a3a3e88
Kyle Davis <kyledavisdev@gmail.com>
2025-06-15 15:05:57 -0500

    Add column to interface

--- app/api/models/Payments.ts
+++ app/api/models/Payments.ts
@@ -3,6 +3,7 @@ export default interface Payment {
   amount_in_pennies: number;
   method: string;
   property_id: number;
+  created_at: string; // ISO 8601 timestamp
   is_active: boolean;
   transaction_issued_by?: string | null;
   transaction_id?: string | null;
