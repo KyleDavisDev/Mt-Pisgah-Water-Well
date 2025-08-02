cd0dfacac3109633a44ddf891932c89896218a69
Kyle Davis <kyledavisdev@gmail.com>
2025-08-02 15:09:27 -0500

    init commit

---
+++ app/api/payments/update/route.ts
@@ -0,0 +1,39 @@
+import { cookies } from "next/headers";
+import { getUsernameFromCookie, validatePermission } from "../../utils/utils";
+import { PaymentRepository } from "../../repositories/paymentRepository";
+
+export async function PUT(req: Request) {
+  if (req.method !== "PUT") {
+    return new Response("Method Not Allowed", { status: 405 });
+  }
+  try {
+    const cookieStore = cookies();
+    const jwtCookie = cookieStore.get("jwt");
+    const username = await getUsernameFromCookie(jwtCookie);
+    await validatePermission(username, "UPDATE_PAYMENT");
+
+    const { id, isActive } = await req.json();
+
+    if (!id || !isActive) {
+      return new Response("Missing required fields", { status: 400 });
+    }
+
+    // Find the payment to be edited
+    const oldPayment = await PaymentRepository.findById(id);
+    if (!oldPayment) return new Response("Cannot find payment record", { status: 404 });
+
+    // Set the new record data
+    const newPayment = { ...oldPayment, is_active: isActive === "true" };
+
+    const updatedRecord = await PaymentRepository.updateAsTransactional(username, oldPayment, newPayment);
+
+    if (!updatedRecord) return new Response("Failed to update payment record", { status: 500 });
+
+    return Response.json({ message: "Success!" });
+  } catch (error) {
+    console.log(error);
+    return new Response("Invalid username or password.", { status: 403 });
+  }
+
+  return new Response("Something went wrong.", { status: 500 });
+}
