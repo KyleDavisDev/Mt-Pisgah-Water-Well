30f3df05a07fd0bf55b288235e980eb98dd7e749
Kyle Davis <kyledavisdev@gmail.com>
2025-08-05 19:54:59 -0500

    update

--- app/admin/dashboard/layout.tsx
+++ app/admin/dashboard/layout.tsx
@@ -6,13 +6,17 @@ import SidebarMenu from "./components/SidebarMenu/SidebarMenu";
 const StyledDiv = styled.div`
   background-color: ${props => props.theme.siteBackgroundColor};
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
   width: 100%;
   min-height: 100vh;
   color: ${props => props.theme.siteFontColor};
 
   @media print {
     background-color: white;
+  }
+
+  @media (min-width: ${props => props.theme.smToMd}) {
+    flex-direction: row;
   }
 `;
 
