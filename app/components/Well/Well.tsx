033b3c47e5597fdf72992d1b7b4b9e7496165638
Kyle Davis <kyledavisdev@gmail.com>
2025-08-18 20:56:53 -0500

    Implement classes

--- app/components/Well/Well.tsx
+++ app/components/Well/Well.tsx
@@ -1,7 +1,5 @@
 import * as React from "react";
 
-import { StyledDiv } from "./WellStyle";
-
 export interface WellProps {
   className?: string;
   children: string | React.JSX.Element | (string | React.JSX.Element)[];
@@ -10,9 +8,19 @@ export interface WellProps {
 
 const Well: React.FC<WellProps> = props => {
   return (
-    <StyledDiv className={props.className} style={props.style}>
+    <div
+      className="
+        max-w-[2000px] w-full mx-0
+        border-1 border-white bg-white rounded-5
+        flex flex-col justify-start
+        transition-all duration-200 ease-linear
+        p-4 box-border
+        [&>a:last-child]:mt-auto [&>a:last-child]:mx-auto [&>a:last-child]:mb-0
+      "
+      style={props.style}
+    >
       {props.children}
-    </StyledDiv>
+    </div>
   );
 };
 
