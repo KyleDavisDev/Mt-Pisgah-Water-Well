218d0cdcdd463b0b57888620b0bdcc3b4551f55f
Kyle Davis <kyledavisdev@gmail.com>
2025-08-24 22:25:23 -0500

    Cleanup for responsiveness

--- app/admin/dashboard/components/ArticleHolder/ArticleHolder.tsx
+++ app/admin/dashboard/components/ArticleHolder/ArticleHolder.tsx
@@ -8,8 +8,14 @@ interface ArticleHolderProps {
 
 export const ArticleHolder = ({ children }: ArticleHolderProps) => {
   return (
-    <div className={"w-full h-screen overflow-scroll"}>
-      <Article size="lg" className={"flex flex-col overflow-scroll rounded-md border border-inputBorder  mt-15 mb-15"}>
+    <div className={"w-full h-screen overflow-scroll pl-[15px] pr-[15px]"}>
+      <Article
+        size="lg"
+        className={`flex flex-col overflow-scroll rounded-md border border-inputBorder
+         mt-[30px] mb-[30px] pl-0 pr-0
+         sm:mt-[60px] sm:mb-[60px]
+         `}
+      >
         <Well>{children}</Well>
       </Article>
     </div>
