4f544822f21f5ddb1e8c202ac352e6b17bb5baeb
Kyle Davis <kyledavisdev@gmail.com>
2025-08-26 21:25:17 -0500

    Move to tailwind

--- app/admin/dashboard/page.tsx
+++ app/admin/dashboard/page.tsx
@@ -1,36 +1,15 @@
 "use client";
 
 import * as React from "react";
-import styled from "../../theme/styled-components";
 import Article from "../../components/Article/Article";
 import Well from "../../components/Well/Well";
 
-const StyledContainer = styled.div`
-  width: 100%;
-  height: 100vh;
-  overflow: scroll;
-`;
-
-const StyledWellContainer = styled.div`
-  margin-top: 1.5rem;
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  flex-wrap: wrap;
-`;
-
-const StyledWellHolder = styled.div`
-  padding: 15px;
-  width: 50%;
-  box-sizing: border-box;
-`;
-
 const Page = (): React.JSX.Element => {
   return (
-    <StyledContainer>
+    <div className={"w-full h-screen overflow-scroll"}>
       <Article size="lg">
-        <StyledWellContainer>
-          <StyledWellHolder>
+        <div className={"mt-6 flex flex-row justify-center flex-wrap"}>
+          <div className={"p-[15px] w-1/2 box-border"}>
             <Well style={{ minHeight: 300 }}>
               <i>Random chart 1</i>
               <img
@@ -39,25 +18,25 @@ const Page = (): React.JSX.Element => {
                 }
               />
             </Well>
-          </StyledWellHolder>
-          <StyledWellHolder>
+          </div>
+          <div className={"p-[15px] w-1/2 box-border"}>
             <Well style={{ minHeight: 300 }}>
               <i>Random chart 2</i>
             </Well>
-          </StyledWellHolder>
-          <StyledWellHolder>
+          </div>
+          <div className={"p-[15px] w-1/2 box-border"}>
             <Well style={{ minHeight: 300 }}>
               <i>Random chart 3</i>
             </Well>
-          </StyledWellHolder>
-          <StyledWellHolder>
+          </div>
+          <div className={"p-[15px] w-1/2 box-border"}>
             <Well style={{ minHeight: 300 }}>
               <i>Random chart 4</i>
             </Well>
-          </StyledWellHolder>
-        </StyledWellContainer>
+          </div>
+        </div>
       </Article>
-    </StyledContainer>
+    </div>
   );
 };
 
