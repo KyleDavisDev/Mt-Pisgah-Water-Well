d55168ce340ea9252b18ac7d05df3cd40741f558
Kyle Davis <kyledavisdev@gmail.com>
2025-08-30 18:43:27 -0500

    init commit

---
+++ app/api/utils/errors.ts
@@ -0,0 +1,28 @@
+export class HttpError extends Error {
+  status: number;
+  constructor(message: string, status: number) {
+    super(message);
+    this.name = this.constructor.name;
+    this.status = status;
+    // Maintains proper stack trace (only needed in V8)
+    if (Error.captureStackTrace) Error.captureStackTrace(this, this.constructor);
+  }
+}
+
+export class BadRequestError extends HttpError {
+  constructor(message = "Bad Request") {
+    super(message, 400);
+  }
+}
+
+export class UnauthorizedError extends HttpError {
+  constructor(message = "Unauthorized") {
+    super(message, 401);
+  }
+}
+
+export class ForbiddenError extends HttpError {
+  constructor(message = "Forbidden") {
+    super(message, 403);
+  }
+}
