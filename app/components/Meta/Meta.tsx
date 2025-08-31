9c6b62b0371cc665bb309e6ce80deddd2f550a9d
Kyle Davis <kyledavisdev@gmail.com>
2025-08-31 14:25:06 -0500

    Update

--- app/components/Meta/Meta.tsx
+++ app/components/Meta/Meta.tsx
@@ -7,7 +7,7 @@ const Meta = () => {
     <Head>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta charSet="utf-8" />
-      <link rel="shortcut icon" href="/static/favicon.png" />
+      <link rel="icon" href="/favicon.ico" sizes="any" />
       <title>Mt Pisgah Well</title>
     </Head>
   );
