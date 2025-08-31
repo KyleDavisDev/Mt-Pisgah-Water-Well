367d224b5fc901d67251dbad03cbce1e51b92697
Kyle Davis <kyledavisdev@gmail.com>
2025-08-31 13:11:44 -0500

    Allow size param

--- app/admin/dashboard/components/ArticleHolder/ArticleHolder.tsx
+++ app/admin/dashboard/components/ArticleHolder/ArticleHolder.tsx
@@ -4,13 +4,14 @@ import Article from "../../../../components/Article/Article";
 
 interface ArticleHolderProps {
   children: any;
+  size?: "xs" | "sm" | "md" | "lg" | undefined;
 }
 
-export const ArticleHolder = ({ children }: ArticleHolderProps) => {
+export const ArticleHolder = ({ children, size }: ArticleHolderProps) => {
   return (
     <div className={"w-full h-screen overflow-scroll pl-[15px] pr-[15px]"}>
       <Article
-        size="lg"
+        size={size ?? "lg"}
         className={`flex flex-col overflow-scroll rounded-md border border-inputBorder
          mt-[30px] mb-[30px] pl-0 pr-0
          sm:mt-[60px] sm:mb-[60px]
