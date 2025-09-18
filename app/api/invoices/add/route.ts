4b7c6ed781feeaaa6b12ab80d4823c58abae5193
Kyle Davis <kyledavisdev@gmail.com>
2025-09-17 20:24:26 -0500

    Remove console log, fix logic error

--- app/api/invoices/add/route.ts
+++ app/api/invoices/add/route.ts
@@ -16,12 +16,11 @@ import { MethodNotAllowedError } from "../../utils/errors";
 export const dynamic = "force-dynamic";
 
 const getPricingFormula = (year: number, month: number) => {
-  console.log(year, month);
-  // In Sept 6, 2025 meeting a new formula was adopted.
+  // On Sept 6 2025, there was a W.S.C. meeting and a new formula was adopted.
   const cutoffYear = 2025;
   const cutoffMonth = 9;
 
-  if (month < cutoffMonth && year <= cutoffYear) {
+  if (year < cutoffYear || (year === cutoffYear && month < cutoffMonth)) {
     return PRICING_FORMULAS["tiered_2025_v1"];
   } else {
     return PRICING_FORMULAS["tiered_2025_September_v1"];
