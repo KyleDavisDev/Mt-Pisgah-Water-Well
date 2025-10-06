c69361c2eb9faa87b8623e12a43aa52f85c7ad76
Kyle Davis <kyledavisdev@gmail.com>
2025-10-05 23:49:20 -0500

    Remove stringify

--- app/api/repositories/invoiceRepository.ts
+++ app/api/repositories/invoiceRepository.ts
@@ -188,7 +188,7 @@ export class InvoiceRepository {
       VALUES (${newData.property_id},
               ${newData.amount_in_pennies},
               ${newData.type},
-              ${db.json(JSON.stringify(newData.metadata))},
+              ${db.json(newData.metadata)},
               ${newData.is_active})
       RETURNING *;
     `;
