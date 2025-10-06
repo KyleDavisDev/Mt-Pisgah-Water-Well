95fabcac6a51ef404699eaa44fe102b8ae3bc964
Kyle Davis <kyledavisdev@gmail.com>
2025-10-05 22:11:59 -0500

    WIP, render items onto table

--- app/admin/dashboard/properties/account-balance/page.tsx
+++ app/admin/dashboard/properties/account-balance/page.tsx
@@ -3,9 +3,11 @@
 import { useState, useRef, useEffect } from "react";
 import { ArticleHolder } from "../../components/ArticleHolder/ArticleHolder";
 import Select from "../../../../components/Select/Select";
+import { formatISODateToUserFriendlyLocal, formatPenniesToDollars } from "../../util";
+import { Link } from "../../../../components/Link/Link";
 
 interface propertyVM {
-  id: string;
+  id: number;
   street: string;
   description: string;
   isActive: string;
@@ -26,16 +28,17 @@ interface invoiceVM {
   propertyId: number;
   amountInPennies: number;
   type: string;
-  metadata: [
-    discounts?: [{ name: string; description: string }],
-    gallons_end?: number,
-    billing_year?: number,
-    formula_used?: string,
-    gallons_used?: number,
-    billing_month?: number,
-    gallons_start?: number,
-    current_balance_in_pennies?: number
-  ];
+  metadata: {
+    discounts: [{ name: string; description: string }];
+    gallons_end: number;
+    billing_year: number;
+    formula_used: string;
+    gallons_used: number;
+    billing_month: number;
+    gallons_start: number;
+    balance_in_pennies_start: number;
+    balance_in_pennies_end: number;
+  };
   createdAt: string;
   isActive: boolean;
 }
@@ -108,13 +111,17 @@ const Page = () => {
       })
       .then(data => {
         // Update state with the fetched data
-        setInvoices(data.payments);
+        setInvoices(data.invoices);
       })
       .catch(error => {
         // Handle fetch errors
         console.error("Error fetching data:", error);
       })
       .finally(() => {});
+  };
+
+  const mergePaymentsAndInvoices = (): (paymentVM | invoiceVM)[] => {
+    return [...payments, ...invoices].sort((a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime());
   };
 
   useEffect(() => {
@@ -125,9 +132,11 @@ const Page = () => {
   }, []);
 
   useEffect(() => {
-    if (selectedProperty && selectedProperty.id && parseInt(selectedProperty.id, 10) > 0) {
-      getPaymentsByPropertyId(selectedProperty.id);
-      getInvoicesByPropertyId(selectedProperty.id);
+    if (selectedProperty && selectedProperty.id && selectedProperty.id > 0) {
+      setInvoices([]);
+      setPayments([]);
+      getPaymentsByPropertyId(selectedProperty.id.toString());
+      getInvoicesByPropertyId(selectedProperty.id.toString());
     }
   }, [selectedProperty]);
 
@@ -151,21 +160,86 @@ const Page = () => {
               ...properties.map(prop => {
                 return {
                   name: `${prop.street} (${prop.homeowner})`,
-                  value: prop.id
+                  value: prop.id.toString()
                 };
               })
             ]}
             onSelect={e => {
               const value = parseInt(e.target.value, 10);
-
-              return setSelectedProperty(properties[value]);
+              setSelectedProperty(properties.find(p => p.id === value));
             }}
             id={"userWhoGathered"}
             label={"Property"}
             showLabel={true}
-            selectedValue={selectedProperty ? selectedProperty.street : defaultPropertySelection.name}
+            selectedValue={selectedProperty ? selectedProperty.id.toString() : defaultPropertySelection.value}
           />
         </div>
+      </div>
+      <div className={"w-full flex flex-row"}>
+        {/* Merge and sort payments and invoices */}
+        {selectedProperty && (payments.length > 0 || invoices.length > 0) ? (
+          <table className={"w-full text-left border-collapse mb-[25px]"}>
+            <thead className={"border-collapse"}>
+              <tr>
+                <th className={"border border-tableBorder text-left p-[8px] table-cell border-collapse"}>Date</th>
+                <th className={"border border-tableBorder text-left p-[8px] table-cell border-collapse"}>Type</th>
+                <th className={"border border-tableBorder text-left p-[8px] table-cell border-collapse"}>Amount</th>
+                <th className={"border border-tableBorder text-left p-[8px] table-cell border-collapse"}>
+                  Method/Description
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {/* Render merged payments and invoices */}
+              {mergePaymentsAndInvoices().map(item => {
+                // Render invoice
+                if ("type" in item && "metadata" in item) {
+                  const invoice = item as invoiceVM;
+                  return (
+                    <tr key={`invoice-${invoice.id}`}>
+                      <td className={"border border-tableBorder p-[8px]"}>
+                        {formatISODateToUserFriendlyLocal(invoice.createdAt)}
+                      </td>
+                      <td className={"border border-tableBorder p-[8px]"}>
+                        Invoice &#x2002;
+                        <Link
+                          href={`/admin/dashboard/invoices/view/${invoice.id}`}
+                          target={"_blank"}
+                          className={"text-primaryThemeColor"}
+                        >
+                          <i>(view)</i>
+                        </Link>
+                      </td>
+                      <td className={"border border-tableBorder p-[8px]"}>
+                        {invoice.amountInPennies === undefined ? "--" : formatPenniesToDollars(invoice.amountInPennies)}
+                      </td>
+                      <td className={"border border-tableBorder p-[8px]"}>{invoice.metadata?.formula_used || "-"}</td>
+                    </tr>
+                  );
+                }
+                // Render payment
+                if ("method" in item) {
+                  const payment = item as paymentVM;
+                  return (
+                    <tr key={`payment-${payment.id}`}>
+                      <td className={"border border-tableBorder p-[8px]"}>
+                        {formatISODateToUserFriendlyLocal(payment.createdAt)}
+                      </td>
+                      <td className={"border border-tableBorder p-[8px]"}>Payment</td>
+                      <td className={"border border-tableBorder p-[8px]"}>
+                        {payment.amountInPennies === undefined ? "--" : formatPenniesToDollars(payment.amountInPennies)}
+                      </td>
+                      <td className={"border border-tableBorder p-[8px]"}>{payment.method}</td>
+                    </tr>
+                  );
+                }
+                return null;
+              })}
+            </tbody>
+          </table>
+        ) : selectedProperty ? (
+          <div className="p-6">Loading...</div>
+        ) : null}
       </div>
     </ArticleHolder>
   );
