20125a483a01f82df68e4104cff5d12f7707e82a
Kyle Davis <kyledavisdev@gmail.com>
2025-10-11 15:49:57 -0500

    Cleanup, update method name

--- app/api/payments/update/route.ts
+++ app/api/payments/update/route.ts
@@ -1,41 +1,36 @@
 import { cookies } from "next/headers";
 import { getUsernameFromCookie, validatePermission } from "../../utils/utils";
 import { PaymentRepository } from "../../repositories/paymentRepository";
-import { ForbiddenError } from "../../utils/errors";
 import { withErrorHandler } from "../../utils/handlers";
+import { BadRequestError, InternalServerError, ResourceNotFoundError } from "../../utils/errors";
 
 // NextJS quirk to make the route dynamic
 export const dynamic = "force-dynamic";
 
 const handler = async (req: Request) => {
-  try {
-    const cookieStore = await cookies();
-    const jwtCookie = cookieStore.get("jwt");
-    const username = await getUsernameFromCookie(jwtCookie);
-    await validatePermission(username, "UPDATE_PAYMENT");
+  const cookieStore = await cookies();
+  const jwtCookie = cookieStore.get("jwt");
+  const username = await getUsernameFromCookie(jwtCookie);
+  await validatePermission(username, "UPDATE_PAYMENT");
 
-    const { id, isActive } = await req.json();
+  const { id, isActive } = await req.json();
 
-    if (!id || !isActive) {
-      return new Response("Missing required fields", { status: 400 });
-    }
-
-    // Find the payment to be edited
-    const oldPayment = await PaymentRepository.findById(id);
-    if (!oldPayment) return new Response("Cannot find payment record", { status: 404 });
-
-    // Set the new record data
-    const newPayment = { ...oldPayment, is_active: isActive === "true" };
-
-    const updatedRecord = await PaymentRepository.updateAsTransactional(username, oldPayment, newPayment);
-
-    if (!updatedRecord) return new Response("Failed to update payment record", { status: 500 });
-
-    return Response.json({ message: "Success!" });
-  } catch (error) {
-    console.log(error);
-    throw new ForbiddenError("Invalid username or password.");
+  if (!id || !isActive) {
+    throw new BadRequestError("Missing required fields");
   }
+
+  // Find the payment to be edited
+  const oldPayment = await PaymentRepository.findById(id);
+  if (!oldPayment) throw new ResourceNotFoundError("Cannot find payment record");
+
+  // Set the new record data
+  const newPayment = { ...oldPayment, is_active: isActive === "true" };
+
+  const updatedRecord = await PaymentRepository.updateAsTransactional(username, oldPayment, newPayment);
+
+  if (!updatedRecord) throw new InternalServerError("Failed to update payment record");
+
+  return Response.json({ message: "Success!" });
 };
 
 export const PUT = withErrorHandler(handler);
