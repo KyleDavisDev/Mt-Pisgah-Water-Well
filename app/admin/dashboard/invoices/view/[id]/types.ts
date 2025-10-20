d98061d3c0043f51cdf98febef0e3a5f89f65a39
Kyle Davis <kyledavisdev@gmail.com>
2025-10-19 20:15:04 -0500

    Comment out

--- app/admin/dashboard/invoices/view/[id]/types.ts
+++ app/admin/dashboard/invoices/view/[id]/types.ts
@@ -1,96 +1,96 @@
-export interface BillUsage {
-  start: number;
-  end: number;
-  usage: number;
-}
-
-export interface BillCharges {
-  baseCharge: string;
-  excessUsageChargeInPennies: number;
-  lateFee: number;
-  otherCharges: number;
-  amountOwingInPennies: number;
-  totalChargeAmountInPennies: number;
-  accountBalanceBeforeInPennies: number;
-  accountBalanceAfterInPennies: number;
-  formula: {
-    description: string;
-    baseFeeInPennies: number;
-    baseGallons: number;
-    usageRateInPennies: number;
-  };
-}
-
-export interface WaterCompanyInfo {
-  name: string;
-  address: string;
-  city: string;
-  state: string;
-  zip: string;
-  phone: string;
-  fax: string;
-  email: string;
-}
-
-export interface HomeownerInfo {
-  name: string;
-}
-
-export interface PropertyInfo {
-  street: string;
-  city: string;
-  state: string;
-  zip: string;
-}
-
-export interface InvoiceDetails {
-  id: string;
-  createdDate: string;
-  billingPeriod: string;
-  waterCompany: WaterCompanyInfo;
-  homeowner: HomeownerInfo;
-  property: PropertyInfo;
-  bill: BillCharges;
-  currentUsage: BillUsage;
-  monthlyUsageHistory: invoiceDTO[];
-}
-
-export interface invoiceDTO {
-  id: string;
-  amountInPennies: number;
-  balanceInPenniesStart: number;
-  balanceInPenniesEnd: number;
-  formula: {
-    description: string;
-    baseFeeInPennies: number;
-    baseGallons: number;
-    usageRateInPennies: number;
-  };
-  gallonsUsed: number;
-  gallonsStart: number;
-  gallonsEnd: number;
-  month: number;
-  year: number;
-  isActive: boolean;
-  createdAt: string;
-}
-
-export interface homeownerDTO {
-  name: string;
-}
-
-export interface propertyDTO {
-  street: string;
-  city: string;
-  state: string;
-  zip: string;
-}
-
-export interface historicalUsageDTO {
-  month: number;
-  year: number;
-  gallonsUsed: string;
-  gallonsStart: string;
-  gallonsEnd: string;
-  amountInPennies: number;
-}
+// export interface BillUsage {
+//   start: number;
+//   end: number;
+//   usage: number;
+// }
+//
+// export interface BillCharges {
+//   baseCharge: string;
+//   excessUsageChargeInPennies: number;
+//   lateFee: number;
+//   otherCharges: number;
+//   amountOwingInPennies: number;
+//   totalChargeAmountInPennies: number;
+//   accountBalanceBeforeInPennies: number;
+//   accountBalanceAfterInPennies: number;
+//   formula: {
+//     description: string;
+//     baseFeeInPennies: number;
+//     baseGallons: number;
+//     usageRateInPennies: number;
+//   };
+// }
+//
+// export interface WaterCompanyInfo {
+//   name: string;
+//   address: string;
+//   city: string;
+//   state: string;
+//   zip: string;
+//   phone: string;
+//   fax: string;
+//   email: string;
+// }
+//
+// export interface HomeownerInfo {
+//   name: string;
+// }
+//
+// export interface PropertyInfo {
+//   street: string;
+//   city: string;
+//   state: string;
+//   zip: string;
+// }
+//
+// export interface InvoiceDetails {
+//   id: string;
+//   createdDate: string;
+//   billingPeriod: string;
+//   waterCompany: WaterCompanyInfo;
+//   homeowner: HomeownerInfo;
+//   property: PropertyInfo;
+//   bill: BillCharges;
+//   currentUsage: BillUsage;
+//   monthlyUsageHistory: invoiceDTO[];
+// }
+//
+// export interface invoiceDTO {
+//   id: string;
+//   amountInPennies: number;
+//   balanceInPenniesStart: number;
+//   balanceInPenniesEnd: number;
+//   formula: {
+//     description: string;
+//     baseFeeInPennies: number;
+//     baseGallons: number;
+//     usageRateInPennies: number;
+//   };
+//   gallonsUsed: number;
+//   gallonsStart: number;
+//   gallonsEnd: number;
+//   month: number;
+//   year: number;
+//   isActive: boolean;
+//   createdAt: string;
+// }
+//
+// export interface homeownerDTO {
+//   name: string;
+// }
+//
+// export interface propertyDTO {
+//   street: string;
+//   city: string;
+//   state: string;
+//   zip: string;
+// }
+//
+// export interface historicalUsageDTO {
+//   month: number;
+//   year: number;
+//   gallonsUsed: string;
+//   gallonsStart: string;
+//   gallonsEnd: string;
+//   amountInPennies: number;
+// }
