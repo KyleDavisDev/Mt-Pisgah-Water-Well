f982881cd7b50433ba3f5bed72316887726018b6
Kyle Davis <kyledavisdev@gmail.com>
2025-11-02 12:40:25 -0600

    init commit

---
+++ app/api/repositories/FeeRepository.ts
@@ -0,0 +1,168 @@
+import { db } from "../utils/db";
+import Fee, { FeeCreate } from "../models/Fee";
+import { AuditRepository } from "./auditRepository";
+import { getStartAndEndOfProvidedMonthAndNextMonth } from "../utils/utils";
+
+export class FeeRepository {
+  /**
+   * Retrieves a single fee by its unique identifier.
+   *
+   * @param {string} id - The unique identifier of the fee to retrieve.
+   * @returns {Promise<Fee | null>} A promise that resolves to the usage fee if found, or null if not found.
+   */
+  static getFeeById = async (id: string): Promise<Fee | null> => {
+    if (!id) return null;
+
+    const [fee] = await db<Fee[]>`
+      SELECT *
+      FROM fees
+      WHERE id = ${id}
+      LIMIT 1;
+    `;
+
+    return fee ?? null;
+  };
+
+  /**
+   * Retrieves all usage fees for the given list of property IDs.
+   *
+   * @param propertyIds - An array of property IDs to filter by.
+   * @param type - The type of fee to retrieve.
+   * @returns A Promise resolving to an array of sorted (created_at desc) Fees.
+   */
+  static getFeesByPropertyIdsAndCategory = async (propertyIds: number[], category: string): Promise<Fee[]> => {
+    if (!propertyIds || propertyIds.length === 0) return [];
+
+    return db<Fee[]>`
+      SELECT *
+      FROM fees
+      WHERE property_id IN ${db(propertyIds)}
+        AND category = ${category}
+        AND is_active = true
+      ORDER BY created_at desc;
+    `;
+  };
+
+  /**
+   * Retrieves all active usage fees for a given year, month, and list of property IDs.
+   *
+   * @param {number} year - The 4-digit billing year (e.g., 2025).
+   * @param {number} month - The billing month (1–12).
+   * @param {number[]} propertyIds - List of property IDs to filter against.
+   *
+   * @returns {Promise<Fee[]>} A promise that resolves to an array of matching usage fees.
+   */
+  static getActiveFeesByYearAndMonthAndPropertyIn = async (
+    year: number,
+    month: number,
+    propertyIds: number[]
+  ): Promise<Fee[]> => {
+    if (!Array.isArray(propertyIds) || propertyIds.length === 0) return [];
+
+    const { startOfMonth, startOfNextMonth } = getStartAndEndOfProvidedMonthAndNextMonth(
+      year.toString(10),
+      month.toString(10)
+    );
+
+    const fees = await db<Fee[]>`
+    SELECT *
+    FROM fees
+    WHERE is_active = true
+      AND property_id IN ${db(propertyIds)}
+        AND created_at >= ${startOfMonth}
+    AND created_at < ${startOfNextMonth}
+  `;
+
+    return fees ?? [];
+  };
+
+  /**
+   * Inserts a new fee record into the database within a transactional context.
+   *
+   * @param {String} user - The user who is doing the inserting.
+   * @param {FeeCreate} newData - The fee data to insert.
+   *
+   * @returns {Promise<Fee | null>} Resolves to the newly created fee record.
+   */
+  static insertNewFeeAsTransactional = async (user: string, newData: FeeCreate): Promise<Fee | null> => {
+    const auditLog = await AuditRepository.addAuditTableRecord({
+      tableName: "fees",
+      recordId: 0,
+      newData: JSON.stringify(newData),
+      actionBy: user,
+      actionType: "INSERT"
+    });
+
+    if (!auditLog) return null;
+
+    let savedFee: Fee | null = null;
+    await db.begin(async db => {
+      const justInsertedFee = await db<Fee[]>`
+      INSERT INTO fees (property_id,
+                        bill_id,
+                            amount_in_pennies,
+                            category,
+                            metadata,
+                            is_active,
+                            created_at)
+      VALUES (${newData.property_id},
+              ${newData.bill_id},
+              ${newData.amount_in_pennies},
+              ${newData.category},
+              ${db.json(newData.metadata)},
+              ${newData.is_active},
+              ${newData.created_at})
+      RETURNING *;
+    `;
+
+      await db`
+      UPDATE audit_log
+      SET is_complete = true,
+          record_id   = ${justInsertedFee[0].id}
+      WHERE id = ${auditLog.id};
+    `;
+
+      savedFee = justInsertedFee[0];
+    });
+
+    return savedFee;
+  };
+
+  /**
+   * Inserts a new fee record into the database within a transactional context.
+   *
+   * @param {String} user - The user who is doing the inserting.
+   * @param {Fee} oldData - The old fee.
+   * @param {Fee} newData - The new fee.
+   *
+   * @returns {Promise<Fee | null>} Resolves to the newly created fee record.
+   */
+  static updateFeeAsTransactional = async (user: string, oldData: Fee, newData: Fee): Promise<Fee | null> => {
+    const auditLog = await AuditRepository.addAuditTableRecord({
+      tableName: "fees",
+      recordId: oldData.id,
+      oldData: JSON.stringify(oldData),
+      newData: JSON.stringify(newData),
+      actionBy: user,
+      actionType: "UPDATE"
+    });
+
+    if (!auditLog) return null;
+
+    await db.begin(async db => {
+      await db`
+      UPDATE fees
+      set is_active = ${newData.is_active}
+      WHERE id = ${oldData.id};
+    `;
+
+      await db`
+        UPDATE audit_log
+        SET is_complete= true
+        WHERE id = ${auditLog.id};
+      `;
+    });
+
+    return newData;
+  };
+}
