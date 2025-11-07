4e73823ad02b1abeddbed214b48144026e2bc571
Kyle Davis <kyledavisdev@gmail.com>
2025-11-06 19:41:52 -0600

    Consolidate logic

--- app/api/fees/createFee.ts
+++ app/api/fees/createFee.ts
@@ -1,11 +1,19 @@
-import { PRICING_FORMULAS } from "../invoices/pricingFormulas";
+import { PRICING_FORMULAS } from "./water/pricingFormulas";
 
-export const getWaterPricingFormula = (year: number, month: number) => {
+export const getWaterPricingFormulaByYearAndMonth = (year: number, month: number) => {
+  // Simple, scrappy way to turn year and month to the expected format.
+  // Validations and/or sanity checks would improve this.
+  const dateString = `${year.toString().padStart(4, "0")}-${month.toString().padStart(2, "0")}-01`;
+  const date = new Date(dateString);
+
+  return getWaterPricingFormulaByDate(date);
+};
+
+export const getWaterPricingFormulaByDate = (providedDate: Date) => {
   // On Sept 6 2025, there was a W.S.C. meeting and a new formula was adopted.
-  const cutoffYear = 2025;
-  const cutoffMonth = 9;
+  const cutoffDate = new Date("2025-06-01");
 
-  if (year < cutoffYear || (year === cutoffYear && month < cutoffMonth)) {
+  if (providedDate < cutoffDate) {
     return PRICING_FORMULAS["tiered_2025_v1"];
   } else {
     return PRICING_FORMULAS["tiered_2025_September_v1"];
