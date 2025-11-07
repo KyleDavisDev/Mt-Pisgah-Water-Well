66a8bdf46c58e50d122f4d0e6d18df0c85688f97
Kyle Davis <kyledavisdev@gmail.com>
2025-11-06 20:26:07 -0600

    Add method to get formula by name

--- app/api/fees/createFee.ts
+++ app/api/fees/createFee.ts
@@ -1,6 +1,7 @@
 import { PRICING_FORMULAS } from "./water/pricingFormulas";
+import { PricingFormula } from "./water/pricingFormulas/types";
 
-export const getWaterPricingFormulaByYearAndMonth = (year: number, month: number) => {
+export const getWaterPricingFormulaByYearAndMonth = (year: number, month: number): PricingFormula => {
   // Simple, scrappy way to turn year and month to the expected format.
   // Validations and/or sanity checks would improve this.
   const dateString = `${year.toString().padStart(4, "0")}-${month.toString().padStart(2, "0")}-01`;
@@ -9,7 +10,7 @@ export const getWaterPricingFormulaByYearAndMonth = (year: number, month: number
   return getWaterPricingFormulaByDate(date);
 };
 
-export const getWaterPricingFormulaByDate = (providedDate: Date) => {
+export const getWaterPricingFormulaByDate = (providedDate: Date): PricingFormula => {
   // On Sept 6 2025, there was a W.S.C. meeting and a new formula was adopted.
   const cutoffDate = new Date("2025-06-01");
 
@@ -18,4 +19,8 @@ export const getWaterPricingFormulaByDate = (providedDate: Date) => {
   } else {
     return PRICING_FORMULAS["tiered_2025_September_v1"];
   }
+};
+
+export const getWaterPricingFormulaByName = (name: string): PricingFormula => {
+  return PRICING_FORMULAS[name];
 };
